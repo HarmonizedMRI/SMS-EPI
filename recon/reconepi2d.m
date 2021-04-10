72f94c9e028b70fc3423df5ff1a9e700065be71b
jon <jfnielse@umich.edu>
2021-04-10 16:53:43 -0400

    On branch develop 	modified:   reconepi2d.m

--- recon/reconepi2d.m
+++ recon/reconepi2d.m
@@ -1,27 +1,32 @@
 % curdir = pwd; cd /opt/matlab/toolbox/irt/; setup; cd(curdir);
 
+% readout gradient trapezoid (with ramp sampling)
 % addpath ../sequence/; fmri2depi;
+% save gx1 gx1
+load gx1
+
+% calculate kspace
 [kx,~] = toppe.utils.g2k([gx1(:) gx1(:)]);  % kx = cycles/cm
-kx = kx - max(kx);
+kx = kx - max(kx)/2;
 
 % System matrix
-%npix = nx;
-%xinit = zeros(npix^2,1);
+nx = 64;
+fov = 22;  % cm
 xinit = zeros(nx,1);
-%nufft_args = {[ny,nx],[6,6],[2*ny,2*nx],[ny/2,nx/2],'table',2^12,'minmax:kb'};
 nufft_args = {[nx],[6],[2*nx],[nx/2],'minmax:kb'};
-mask = true(nx,1); % Mask for support (image space)
-L = 6;
+mask = true(nx,1);
 A = Gmri([fov(1)*kx(:)],mask,'nufft',nufft_args);
-%A = Gmri([fov(1)*kx(:) fov(2)*ky(:)],mask,'nufft',nufft_args);
 
 % Density compensation function
-dcf = gx1(2:(end-1))/max(gx1);
+dcf = gx1(1:(end-2))/max(gx1);  % simply the gradient (kspace velocity)
 
 % Test 
 xtrue = zeros(nx,1);
 xtrue(nx/4:(3*nx/4)) = 1;
 xtrue(3*nx/8:(5*nx/8)) = 1.5;
 d = A * xtrue;               % 'acquired' data
-m = A' * (d(:) .* dcf(:));   % reconstruct (perform inufft along x)
-plot(abs(m))
+xhat = A' * (d(:) .* dcf(:));   % reconstruct (perform inufft along x)
+
+subplot(131); plot(xtrue); title('true object');
+subplot(132); plot(abs(xhat)); title('reconstructed');
+subplot(133); plot(angle(xhat)); title('angle');
