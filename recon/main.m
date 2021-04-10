83c9c6de2b748720f60fee77d4ed0a9e1a355a4e
jon <jfnielse@umich.edu>
2021-04-10 19:50:10 -0400

    On branch develop 	modified:   main.m

--- recon/main.m
+++ recon/main.m
@@ -1,29 +1,33 @@
 % curdir = pwd; cd /opt/matlab/toolbox/irt/; setup; cd(curdir);
 
-% 4/10/21 commit 69bb5e49ac2b19c19dccc0ffb27e52eb42d187ec (develop branch)
+%% 4/10/21 commit 69bb5e49ac2b19c19dccc0ffb27e52eb42d187ec (develop branch)
+
+% load data
 pfile = 'P30720.7'; 
 [dat, rdb_hdr] = toppe.utils.loadpfile(pfile); % dat = [8852 1 20 1 18]
 dat = flipdim(dat,1); % yikes
+
+% get readout
 addpath ../sequence/
 fmri2depi;   % create gpre, gx1, gx. nx = ny = 64; fov = 26; etc
+[kx,ky] = toppe.utils.g2k([gx(:) gy(:)]);  % kx = cycles/cm
+kx = [kx; zeros(length(gx)-length(kx),1)];
 
-% get data for one echo
+% get data for all echoes in first frame (for which gy is off)
 coil = 1;
 slice = 3;
 frame = 1;
-echo = 2;   % EPI echo (not dabecho)
-istart = length(gpre) + (echo-1)*length(gx1) + 1;
-istop = istart + length(gx1) - 1;
-dat = dat(istart:istop, coil, slice, 1, frame); 
 
-% kspace
-[kx,ky] = toppe.utils.g2k([gx(:) gy(:)]);  % kx = cycles/cm
-kx = kx(istart:istop);
+for echo = 1:ny   % EPI echo (not dabecho)
+	istart = length(gpre) + (echo-1)*length(gx1) + 1;
+	istop = istart + length(gx1) - 1;
+	dat1 = dat(istart:istop, coil, slice, 1, frame); 
+	kx1 = kx(istart:istop);
+	x(:,echo) = reconecho(dat1, kx1, nx, fov, gx1);
+end
 
-x = reconecho(dat, kx, nx, fov, gx1);
-
-subplot(121); plot(abs(x)); title('reconstructed');
-subplot(122); plot(angle(x)); title('angle');
+subplot(121); im(abs(x)); title('mag');
+subplot(122); im(angle(x(:,1:2:end)./x(:,2:2:end)), [-pi/3 pi/3]); colorbar;  % paired phase difference
 
 return
 
