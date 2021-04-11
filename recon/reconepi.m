fbf372fff3f2650b9480ab6fa64cc0a256e62781
jon <jfnielse@umich.edu>
2021-04-10 20:57:35 -0400

    On branch develop 	modified:   reconepi.m

--- recon/reconepi.m
+++ recon/reconepi.m
@@ -2,8 +2,8 @@ function x = reconepi(dat, kx, nx, fov, gx)
 % 2D EPI reconstruction.
 % Uses nufft along x (ramp sampling), and ift in y
 %
-% dat: [nt ny]   
-% kx:  [nt 1]  (cycles/cm)
+% dat: [nt ny]   data (complex)   
+% kx:  [nt ny]   cycles/cm
 % nx   image size (int)
 % fov  cm (assumes square fov)
 % gx   [nt 1]  readout gradient (typically a trapezoid) for one echo in EPI train
@@ -12,7 +12,7 @@ function x = reconepi(dat, kx, nx, fov, gx)
 
 x = zeros(nx,ny);
 
-% inufft along readout
+% inverse nufft along readout
 for iy = 1:ny
 	x(:,iy) = reconecho(dat(:,iy), kx(:,iy), nx, fov, gx);
 end
@@ -22,13 +22,3 @@ x = fftshift(fft(fftshift(x,2), [], 2),2);
 
 return
 
-% Test 
-xtrue = zeros(nx,1);
-xtrue(nx/4:(3*nx/4)) = 1;
-xtrue(3*nx/8:(5*nx/8)) = 1.5;
-d = A * xtrue;               % 'acquired' data
-xhat = A' * (d(:) .* dcf(:));   % reconstruct (perform inufft along x)
-
-subplot(131); plot(xtrue); title('true object');
-subplot(132); plot(abs(xhat)); title('reconstructed');
-subplot(133); plot(angle(xhat)); title('angle');
