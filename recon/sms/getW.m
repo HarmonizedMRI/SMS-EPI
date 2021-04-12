d0c221e6ef47c2912b1ef20f39163f35e29e80df
jon <jfnielse@umich.edu>
2021-04-12 17:30:24 -0400

    On branch develop 	modified:   getW.m

--- recon/sms/getW.m
+++ recon/sms/getW.m
@@ -1,6 +1,6 @@
-function getW(ideltax, ideltay)
+function getW(ideltax, ideltay, n)
 %
-% I_c = (S_c*W)*x
+% Image space model: I_c = (S_c*W)*x
 %
 % I_c    [N 1]      collapsed 2d coil image]; N = n*n; image size = [n n]
 % S_c    [N MB*N]   [S_c_1' ... S_c_MB'] where S_c_l is coil sensitivity vector [N 1]
@@ -8,5 +8,7 @@ function getW(ideltax, ideltay)
 % W      [MB*N N]   binary mask/indexing matrix that combines aliased pixels across slices
 % x      [N 1]      object to be reconstructed
 % 
+% ideltax   [MB 1]   x location (row index) of pixels aliasing into I_c
+% ideltay   [MB 1]   y location (column index) of pixels aliasing into I_c
 
 
