efc3113653551659de50260db9cd450109b54e35
jon <jfnielse@umich.edu>
2021-04-13 09:15:23 -0400

    On branch develop 	modified:   README.md 	modified:   getW.m

--- recon/sms/README.md
+++ recon/sms/README.md
@@ -6,7 +6,7 @@ I_c = (S_c*W)*x    [N 1]      collapsed 2d coil image]; N = n*n; image size = [n
 
 S_c    [N MB*N]   S_c(1,:) = [S_c_1' ... S_c_MB'] where S_c_l is coil sensitivity map [N 1]
                   Each row is identical.
-W      [MB*N N]   binary mask/indexing matrix that combines aliased pixels across slices
+W      [MB*N N]   indicator/indexing matrix indicating aliased pixels across slices
 x      [N 1]      object to be reconstructed
 I = S*W*x
 
--- recon/sms/getW.m
+++ recon/sms/getW.m
@@ -1,7 +1,17 @@
-function getW(ideltax, ideltay, n)
+function W = getW(ideltax, ideltay, n)
 %
 % n         [1 1]    int. image matrix size is [n n]
 % ideltax   [MB 1]   x location (row index) of pixels aliasing into I_c
 % ideltay   [MB 1]   y location (column index) of pixels aliasing into I_c
 
+MB = size(ideltax, 1);  % number of simultaneous slices (multiband factor)
+
+N = n^2;
+
+W = spalloc(MB*N, N, N);
+IJ = sub2ind([n n], ideltax, ideltay)
+W(IJ(1)
+for isl = 1:MB
+	i = IJ(isl)
+	W(
 
