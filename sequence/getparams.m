a206eaf17d0dd9fac0bbbd576315b680816e0497
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2021-04-18 16:52:07 -0400

    On branch develop 	modified:   getparams.m

--- sequence/getparams.m
+++ sequence/getparams.m
@@ -2,8 +2,8 @@ function [seq, system] = getparams
 %
 % Define common acquisition parameters and system hardware parameters
 
-seq.nx = 90; seq.ny = seq.nx;   % matrix size
-seq.fov = 21.4;          % in-plane fov (cm)
+seq.nx = 64; seq.ny = seq.nx;   % matrix size
+seq.fov = 18;          % in-plane fov (cm)
 seq.slThick = seq.fov/seq.nx;   % isotropic voxels
 
 % Scan plane prescription (rotation and slice offset)
