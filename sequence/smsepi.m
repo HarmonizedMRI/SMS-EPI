ca2268a9066796ade80ba6eeb13ee270d7ffa2b9
jon <jfnielse@umich.edu>
2021-04-17 21:17:43 -0400

    On branch develop 	modified:   getparams.m 	modified:   smsepi.m

--- sequence/smsepi.m
+++ sequence/smsepi.m
@@ -57,13 +57,14 @@ gpre = gpre(1:(end-1)); % remove 0 at end
 
 % readout trapezoid
 % Allow ramp sampling, and violate Nyquist slightly near kmax for now.
-gx1 = toppe.utils.trapwave2(area, system.ge.maxGrad, system.ge.maxSlew, system.ge.raster*1e3);
+gxslew = 0.8*system.ge.maxSlew;  % reduce PNS
+gx1 = toppe.utils.trapwave2(area, system.ge.maxGrad, gxslew, system.ge.raster*1e3);
 esp = length(gx1)*system.ge.raster*1e3;   % echo spacing (ms)
 if esp > fbesp(1) & esp < fbesp(2)
 	% Reduce maxGrad until echo spacing is outside forbidden range
 	for s = 1:-0.02:0.1
 		mxg = s*system.ge.maxGrad;
-		gx1 = toppe.utils.trapwave2(area, mxg, system.ge.maxSlew, system.ge.raster*1e3);
+		gx1 = toppe.utils.trapwave2(area, mxg, gxslew, system.ge.raster*1e3);
 		if length(gx1)*system.ge.raster*1e3 > fbesp(2)
 			esp = length(gx1)*system.ge.raster*1e3; 
 			break;
