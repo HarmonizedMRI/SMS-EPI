22aa32e702186e9486130c5b476923cbc7f7ee4c
jon <jfnielse@umich.edu>
2021-04-19 18:07:10 -0400

    On branch develop 	modified:   setup.m

--- sessions/fbirn_um_18Apr2021/setup.m
+++ sessions/fbirn_um_18Apr2021/setup.m
@@ -1,6 +1,7 @@
 addpath ~/gitlab/fMRI/toppe/
 curdir = pwd; cd ~/github/mirt; setup; cd(curdir);
-
 % Add the following to ~/.bashrc:
 % TOOLBOX_PATH=/home/jon/Programs/bart-0.7.00
 addpath(strcat(getenv('TOOLBOX_PATH'), '/matlab'));
+
+addpath ~/github/HarmonizedMRI/SMS-EPI/recon      % reconecho.m, recon2depi.m, recon3dcart.m
