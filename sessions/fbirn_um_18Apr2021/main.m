cafd6b59f86798f041ffe6eb34a1bd487d4845ec
jon <jfnielse@umich.edu>
2021-04-19 14:28:03 -0400

    On branch develop 	modified:   main.m

--- sessions/fbirn_um_18Apr2021/main.m
+++ sessions/fbirn_um_18Apr2021/main.m
@@ -2,7 +2,16 @@
 % sensitivity maps (and later: B0 field map)
 %d = toppe.utils.loadpfile('~/tmp/smsepi/P_gre3d.7');
 pfile = '~/tmp/smsepi/P_gre3d.7';
-[ims imsos d] = toppe.utils.recon3dft(pfile, 'readoutFile', '~/tmp/smsepi/readout_gre3d.mod', 'echo', 1);
-% size(d) = [256    64    64    32] 
-%tic; sens2 = bart('ecalib', d(2:4:end,:,:,:)); toc;  % takes 
+[ims imsos d] = toppe.utils.recon3dft(pfile, ...   % size(d) = [256    64    64    32] 
+	'readoutFile', '~/tmp/smsepi/readout_gre3d.mod', ...
+	'echo', 1);
+%tic; sens_bart = bart('ecalib', d(2:4:end,:,:,:)); toc;  % takes 19 min
+d = d(2:4:end,:,:,:);  % oprbw = 31.25 kHz (decimation = 4)
+calsize = 20;
+n = size(d,2); % matrix size (assumed isotropic)
+r = (n/2-calsize/2):(n/2+calsize/2-1);
+fprintf('getting bart sens maps...');
+%tic; sens_bart = bart('ecalib -r 20', d); toc;   % takes 
+fprintf('\n');
+% save sens_bart sens_bart
 
