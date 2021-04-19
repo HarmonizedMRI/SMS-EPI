d354f8b9b62311b0c99577dfea4d0761364d3805
jon <jfnielse@umich.edu>
2021-04-19 13:47:49 -0400

    On branch develop 	modified:   main.m 	modified:   setup.m

--- sessions/fbirn_um_18Apr2021/main.m
+++ sessions/fbirn_um_18Apr2021/main.m
@@ -2,5 +2,7 @@
 % sensitivity maps (and later: B0 field map)
 %d = toppe.utils.loadpfile('~/tmp/smsepi/P_gre3d.7');
 pfile = '~/tmp/smsepi/P_gre3d.7';
-[ims imsos] = toppe.utils.recon3dft(pfile, 'readoutFile', '~/tmp/smsepi/readout_gre3d.mod', 'echo', 1);
+[ims imsos d] = toppe.utils.recon3dft(pfile, 'readoutFile', '~/tmp/smsepi/readout_gre3d.mod', 'echo', 1);
+% size(d) = [256    64    64    32] 
+%tic; sens2 = bart('ecalib', d(2:4:end,:,:,:)); toc;  % takes 
 
