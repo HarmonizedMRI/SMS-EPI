ec35151d00f1b7cf01b1c51ec2df846e94484f59
jon <jfnielse@umich.edu>
2021-04-20 10:31:15 -0400

    On branch develop 	modified:   getAsms.m

--- recon/getAsms.m
+++ recon/getAsms.m
@@ -1,5 +1,5 @@
-function A = getAsense(kmask, imask, sens)
-% function A = getAsense(arg)
+function A = getAsms(IZ, imask, sens)
+% function A = getAsms(arg)
 %
 % Undersampled multi-coil 3D Cartesian system matrix
 %
@@ -7,33 +7,37 @@ function A = getAsense(kmask, imask, sens)
 % kmask    [nx ny nz]      logical mask indicating sampled locations
 % sens     [nx ny nz nc]   coil sensitivity maps 
 
-arg.imsize = size(imask);
-arg.kmask = kmask;
+arg.IZ = IZ;
 arg.imask = imask;
 arg.sens = sens;
 
+[arg.nx arg.ny arg.mb] = size(imask);
+
 arg.nc = size(arg.sens,4);
-arg.nt = sum(arg.kmask(:));    % number of acquired samples (per coil)
-%arg.np = prod(arg.imsize);    % number of spatial positions (voxels)
 arg.np = sum(arg.imask(:));    % number of spatial positions (voxels)
 
+arg.nt = arg.nx*arg.ny*arg.nc;  % number of samples per coil
+
 A = fatrix2('arg', arg, ...
-	'idim', [arg.np], ...      % size of A'*y
+	'idim', [arg.np], ...             % size of x
 	'odim', [arg.nt*arg.nc], ...      % size of A*x
 	'forw', @A_forw, 'back', @A_back);
 	%'mask', true(arg.np,1), ...
 
 return
 
-% x  [arg.np 1] 
-% y  [arg.nc*arg.nt 1]
+% x  [arg.np] 
+% y  [arg.nc*arg.nt]
 function y = A_forw(arg, x)
-	%x = reshape(x, arg.imsize).*arg.imask;
 	x = embed(x, arg.imask);
-	y = zeros(arg.nt*arg.nc,1);
+	%y = zeros(arg.nt*arg.nc,1);
+	y3d = zeros(arg.nx, arg.ny, arg.nc);
 	for ic = 1:arg.nc
 		tmp = fftshift(fftn(fftshift(arg.sens(:,:,:,ic).*x)));
-		y(((ic-1)*arg.nt+1):(ic*arg.nt)) = tmp(arg.kmask);
+		for iy = 1:arg.ny
+			y3d(:,iy,ic) = tmp(:,iy,IZ(iy));
+   	end
+		%y(((ic-1)*arg.nt+1):(ic*arg.nt)) = tmp(arg.kmask);
 	end
 return
 
@@ -42,8 +46,6 @@ function x = A_back(arg, y)
 	for ic = 1:arg.nc
 		tmp = embed(y(((ic-1)*arg.nt+1):(ic*arg.nt)), arg.kmask);
 		tmp = conj(arg.sens(:,:,:,ic)).*fftshift(ifftn(fftshift(tmp)));
-		%x = x + tmp.*arg.imask;
 		x = x + tmp(arg.imask);
 	end
-	%x = reshape(x, arg.np, []); 
 return;
