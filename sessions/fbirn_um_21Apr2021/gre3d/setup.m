df921f297222eb2bc52fd1d01171551d7dfdb7e1
jon <jfnielse@umich.edu>
2021-04-22 17:39:50 -0400

    On branch develop 	modified:   setup.m

--- sessions/fbirn_um_21Apr2021/gre3d/setup.m
+++ sessions/fbirn_um_21Apr2021/gre3d/setup.m
@@ -5,5 +5,3 @@ curdir = pwd; cd ~/github/mirt; setup; cd(curdir);
 setenv('TOOLBOX_PATH', '/home/jon/Programs/bart-0.7.00'); % if getenv doesn't find it
 addpath(strcat(getenv('TOOLBOX_PATH'), '/matlab'));
 % addpath('/home/jon/Programs/bart-0.7.00/matlab');
-
-addpath ~/github/HarmonizedMRI/SMS-EPI/recon      % reconecho.m, recon2depi.m, recon3dcart.m
