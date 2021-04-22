1d01a325b4b39432ac6efd552ec899a5765ac972
jon <jfnielse@umich.edu>
2021-04-22 17:50:40 -0400

    On branch develop 	modified:   main.m

--- sessions/fbirn_um_21Apr2021/smsepi/main.m
+++ sessions/fbirn_um_21Apr2021/smsepi/main.m
@@ -1,26 +1,7 @@
-% sensitivity maps (and later: B0 field map)
-if 0
-	pfile = 'P_gre3d.7';
-	system('tar xf gre3d.tar readout.mod');
-	[ims imsos d] = toppe.utils.recon3dft(pfile, ...   % size(d) = [256 64 64 32] = [nfid ny nz ncoils]
-		'readoutFile', 'readout.mod', ...
-		'flipfid', true, ...
-		'flipim', false, ...
-		'echo', 1);
-
-	d = d(2:4:end,:,:,:);  % oprbw = 31.25 kHz (decimation = 4)
-	fprintf('getting bart sens maps...');
-	tic; sens_bart = bart('ecalib -r 20', d); toc;   % takes 14 min
-	fprintf('\n');
-	sens_bart = sens_bart(:,:,:,:,1);
-	save sens_bart sens_bart
-else
-	load sens_bart
-	sens = sens_bart;
-	clear sens_bart
-end
-
-return;
+%% sensitivity maps
+load ../gre3d/sens_bart
+sens = sens_bart;
+clear sens_bart
 
 ncoils = size(sens,4);
 
