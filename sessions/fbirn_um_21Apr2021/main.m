c220fb82588d4b25b968a9af53d9a95b7979b9e1
jon <jfnielse@umich.edu>
2021-04-22 15:00:57 -0400

    On branch develop 	new file:   main.m 	new file:   setup.m

---
+++ sessions/fbirn_um_21Apr2021/main.m
@@ -0,0 +1,55 @@
+% sensitivity maps (and later: B0 field map)
+if 0
+	pfile = 'P_gre3d.7';
+	system('tar xf gre3d.tar readout.mod');
+	[ims imsos d] = toppe.utils.recon3dft(pfile, ...   % size(d) = [256 64 64 32] = [nfid ny nz ncoils]
+		'readoutFile', 'readout.mod', ...
+		'flipfid', true, ...
+		'flipim', false, ...
+		'echo', 1);
+
+	d = d(2:4:end,:,:,:);  % oprbw = 31.25 kHz (decimation = 4)
+	fprintf('getting bart sens maps...');
+	tic; sens_bart = bart('ecalib -r 20', d); toc;   % takes 14 min
+	fprintf('\n');
+	sens_bart = sens_bart(:,:,:,:,1);
+	save sens_bart sens_bart
+else
+	load sens_bart
+	sens = sens_bart;
+	clear sens_bart
+end
+
+return;
+
+ncoils = size(sens,4);
+
+% matrix size for reconstruction
+mb = 4; % multiband/sms factor (number of simultaneous slices)
+[nx ny] = size(sens(:,:,1,1));
+imsize = [nx ny mb];
+
+% pick out slices from sensitivity map
+slSep = 5;  % cm (see smsepi.m)
+slThick = 0.2812;
+isl = round(slSep/slThick);
+nz = size(sens,3);
+IZmb = [ nz/2-isl, nz/2, nz/2+isl-1, nz];
+sens = sens(:,:,IZmb,:);
+
+% blipped CAIPI sampling pattern
+IZ = caipi(ny,3,1);  % NB! Acquisition was mb=3, so kz=4 not sampled (recon requires mb=even)
+
+% image support
+imask = true(imsize);
+imask(:,:,end) = false;
+
+% acquired data
+dataprep;  % dcart = [nx*ny ncoils]
+
+% reconstruct
+tol = 1e-6;
+xhat = reconsms(dcart(:), IZ, imask, sens, tol);
+im(xhat);
+
+return
