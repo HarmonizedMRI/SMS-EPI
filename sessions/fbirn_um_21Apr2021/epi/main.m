81154af41932ad902e4922e4642f504033be2dff
jon <jfnielse@umich.edu>
2021-04-22 17:20:06 -0400

    On branch develop 	modified:   main.m

--- sessions/fbirn_um_21Apr2021/epi/main.m
+++ sessions/fbirn_um_21Apr2021/epi/main.m
@@ -1,14 +1,63 @@
 % curdir = pwd; cd /opt/matlab/toolbox/irt/; setup; cd(curdir);
 addpath ~/github/pulseq/matlab
 
-%% EPI correction parameters
-dly = 0*0.16;  % fraction of 4us sample
-th0 = 0*0.2;   % odd/even dc phase offset
+%% Load 2D EPI data
+% load data
+pfile = 'P_fmri2depi.7';  
+[dat, rdb_hdr] = toppe.utils.loadpfile(pfile); % dat = [8852 1 20 1 18]
+dat = flipdim(dat,1); % as usual
 
-% frame and slice to reconstruct
-rec.frame = 8;  % gx and gy on, positive sign
-rec.slice = 32;
-rec.coil  = 10;
+% Demodulate by observed B0 eddy current, which are mostly linear.
+% This should help correct for image-domain spatial shift.
+%dat = bsxfun(@times, exp(-1i*b0(:)), dat);
+
+%% get sequence
+if 0
+cd tmp
+fmri2depi;   % gpre, gx1, gx. nx = ny = 64; fov = 26; etc
+cd ..
+[kx,ky] = toppe.utils.g2k([gx(:) gy(:)]);  % kx = cycles/cm
+kx = [kx; zeros(length(gx)-length(kx),1)];
+save scanparams.mat gpre gx1 gx nx ny fov kx
+else
+load scanparams.mat
+end
+
+%% reconstruct
+coil = 10;
+slice = 32;
+frame = 8;   % part of calibration frames (gx and gy both on, positive)
+
+dly = 0.0;
+th0 = 0.12;
+x = recon2depiraw(dat(:,coil,slice,1,frame), ...
+	kx, [nx ny], fov, length(gpre), length(gx1), ...
+	dly, th0);
+
+im(abs(x));
+return;
+
+%% EPI correction parameters
+coil = 10;
+slice = 32;
+frame = 8;   % part of calibration frames (gx and gy both on, positive)
+dly = [0 0]; %-0.2:0.02:0.2;  % fraction of 4us sample
+th0 = -0.3:0.02:0.3;  % odd/even dc phase offset
+load bg ; % background ROI
+clear gnr
+for idly = 1:length(dly)
+	fprintf('.');
+	for ith0 = 1:length(th0)
+		x = recon2depiraw(dat(:,coil,slice,1,frame), ...
+			kx, [nx ny], fov, length(gpre), length(gx1), ...
+			dly(idly), th0(ith0));
+		gnr(idly,ith0) = mean(abs(x(bg)));
+	end
+end
+fprintf('\n');
+surf(gnr);
+return;
+
 
 %% Get B0 eddy current
 if 0
@@ -37,38 +86,16 @@ b0 = (angle(dp.*dn));
 end
 
 
-%% Load 2D EPI data and reconstruct
-% load data
-pfile = 'P_fmri2depi.7';  
-[dat, rdb_hdr] = toppe.utils.loadpfile(pfile); % dat = [8852 1 20 1 18]
-dat = flipdim(dat,1); % as usual
-
-% Demodulate by observed B0 eddy current, which are mostly linear.
-% This should help correct for image-domain spatial shift.
-%dat = bsxfun(@times, exp(-1i*b0(:)), dat);
-
-% get sequence 
-if 0
-cd tmp
-fmri2depi;   % gpre, gx1, gx. nx = ny = 64; fov = 26; etc
-cd ..
-[kx,ky] = toppe.utils.g2k([gx(:) gy(:)]);  % kx = cycles/cm
-kx = [kx; zeros(length(gx)-length(kx),1)];
-save scanparams.mat gpre gx1 gx nx ny fov kx
-else
-load scanparams.mat
-end
-
 % fix zero at end of kx
 %kx(end) = 1.01*kx(end-2);
 %kx(end-1) = 1.01*kx(end-2);
 
 % apply temporal shift (odd/even linear phase correction)
 nt = length(kx);
-%kx = interp1(1:nt, kx, (1:nt)-dly);
-tmp = dat(:,rec.coil,rec.slice,1,rec.frame);
-tmp = interp1(1:nt, tmp, (1:nt)+dly);
-dat(:,rec.coil,rec.slice,1,rec.frame) = tmp;
+kx = interp1(1:nt, kx, (1:nt)-dly);
+%tmp = dat(:,rec.coil,rec.slice,1,rec.frame);
+%tmp = interp1(1:nt, tmp, (1:nt)+dly);
+%dat(:,rec.coil,rec.slice,1,rec.frame) = tmp;
 
 % Get data for desired frame/slice and reshape,
 % and odd/even echo calibration data.
