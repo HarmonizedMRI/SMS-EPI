b80b335e8674399ba27bb35f457b20d4ce3c1efe
jon <jfnielse@umich.edu>
2021-04-22 16:10:26 -0400

    On branch develop 	modified:   recon2depi.m

--- recon/recon2depi.m
+++ recon/recon2depi.m
@@ -1,21 +1,27 @@
-function x = recon2depi(dat, kx, nx, fov, gx)
-% 2D EPI reconstruction.
+function x = recon2depi(dat, kxo, kxe, nx, fov)
+% 2D EPI reconstruction (singe coil).
 % Does nufft along x (ramp sampling), and ift in y.
 % Assumes that dat/kx has already undergone odd/even echo correction.
 %
-% dat: [nt ny]   data (complex)   
-% kx:  [nt ny]   cycles/cm
+% dat:  [nt ny]  data (complex)   
+% kxo:  [nt]     (cycles/cm) sampling locations for odd echoes
+% kxe:  [nt]     (cycles/cm) sampling locations for even echoes
 % nx   image size (int)
 % fov  cm (assumes square fov)
-% gx   [nt 1]  readout gradient (typically a trapezoid) for one echo in EPI train
 
 [nt ny] = size(dat);
 
 x = zeros(nx,ny);
 
 % inverse nufft along readout
-for iy = 1:ny
-	x(:,iy) = reconecho(dat(:,iy), kx(:,iy), nx, fov, gx);
+% first get odd/even Gmri matrix
+[~,Ao,dcfo] = reconecho([], nx, [], [], kxo(:), fov); % odd echoes
+[~,Ae,dcfe] = reconecho([], nx, [], [], kxe(:), fov); % even echoes
+for iy = 1:2:ny
+	x(:,iy) = reconecho(dat(:,iy), nx, Ao, dcfo);
+end
+for iy = 2:2:ny
+	x(:,iy) = reconecho(dat(:,iy), nx, Ae, dcfe);
 end
 
 % ift along pe direction
