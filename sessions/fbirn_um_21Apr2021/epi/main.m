968cb417106c5c0b0a2391881ea0bb797b8b8fea
jon <jfnielse@umich.edu>
2021-04-23 15:20:47 -0400

    On branch develop 	modified:   main.m 	modified:   setup.m

--- sessions/fbirn_um_21Apr2021/epi/main.m
+++ sessions/fbirn_um_21Apr2021/epi/main.m
@@ -1,16 +1,15 @@
 % curdir = pwd; cd /opt/matlab/toolbox/irt/; setup; cd(curdir);
 
-%% Load 2D EPI data
+%% Load 2D EPI data and kspace sampling locations
+
+if false
+
 % load data
 pfile = 'P_fmri2depi.7';  
 [dat, rdb_hdr] = toppe.utils.loadpfile(pfile); % dat = [nt ncoils nslices 1 nframes]
 dat = flipdim(dat,1); % as usual
 
-% Demodulate by observed B0 eddy current, which are mostly linear.
-% This should help correct for image-domain spatial shift.
-%dat = bsxfun(@times, exp(-1i*b0(:)), dat);
-
-%% get sequence info
+% reshape into [nt ny ncoils] and get odd/even kspace locations
 if 0
 cd tmp
 addpath ~/github/pulseq/matlab
@@ -22,9 +21,54 @@ save scanparams.mat gpre gx1 gx nx ny fov kx
 else
 load scanparams.mat
 end
+npre = length(gpre);
+ntrap = length(gx1);
+[d2d, kxo, kxe] = gedatreshape(dat, kx, npre, ntrap, ny);
+
+% Demodulate by observed B0 eddy current, which are mostly linear.
+% This should help correct for image-domain spatial shift.
+%dat = bsxfun(@times, exp(-1i*b0(:)), dat);
+
+% Gmri objects for inverse nufft (ramp sampling)
+[~,Ao,dcfo] = reconecho([], nx, [], [], kxo(:), fov); % odd echoes
+
+end
+
+
+%% Estimate odd/even phase difference (for each slice)
+% Calibration data:
+% frame(s)       gy    gx
+% 1, nframes-4   off   positive
+% 2, nframes-3   off   negative
+% 3, nframes-2   on    positive
+% 4, nframes-1   on    negative
+% 5, nframes     off   off
+
+slice = 32;
+coil = 10;
+
+% Get odd-only and even-only images from frames 3 and 4
+do = 0*d2d(:,:,1,1,1);
+do(:,1:2:end)  = d2d(:,1:2:end,coil,slice,3);
+do(:,2:2:end) = d2d(:,2:2:end,coil,slice,4);
+xo = recon2depi(do, kxo, kxo, nx, fov);
+de = 0*d2d(:,:,1,1,1);
+de(:,1:2:end)  = d2d(:,1:2:end,coil,slice,4);
+de(:,2:2:end) = d2d(:,2:2:end,coil,slice,3);
+xe = recon2depi(de, kxe, kxe, nx, fov);
+im(angle(xe./xo), 1.0*[-1 1]);
+
+return;
+
 
 %% reconstruct
 frame = 8;   % part of calibration frames (gx and gy both on, positive)
+coil = 10;
+slice = 32;
+x = recon2depi(d2d(:,:,coil,slice,frame), kxo, kxe, nx, fov);
+im(x)
+
+return;
 
 dly = 0.0;
 th0 = 0.12;
