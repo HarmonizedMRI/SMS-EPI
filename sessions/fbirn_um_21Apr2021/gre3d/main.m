343d87960c2acbaf140d7f24d1fec408a56b905e
jon <jfnielse@umich.edu>
2021-04-27 10:40:38 -0400

    On branch develop 	modified:   main.m

--- sessions/fbirn_um_21Apr2021/gre3d/main.m
+++ sessions/fbirn_um_21Apr2021/gre3d/main.m
@@ -1,6 +1,6 @@
 pfile = 'P_gre3d.7';
 
-% make sensitivity maps (and later: B0 field map)
+%% make sensitivity maps (and later: B0 field map)
 if false
 system('tar xf gre3d.tar readout.mod');
 [ims imsos d] = toppe.utils.recon3dft(pfile, ...   % size(d) = [256 64 64 32] = [nfid ny nz ncoils]
@@ -17,16 +17,25 @@ sens_bart = sens_bart(:,:,:,:,1);
 save sens_bart sens_bart
 end
 
-%% do 3d nufft recon (as a check)
+%% do 3d sense nufft recon (as a check)
+
+% sens maps
+% flip to match nufft (Gmri) orientation
+load sens_bart;
+sens = sens_bart;
+sens = flipdim(sens,2);
+sens = flipdim(sens,3);
 
 % data
 [~,gx,gy,gz,desc,hdrint] = toppe.readmod('readout.mod');  % from gre3d.tar
 npre = hdrint(1);
 nro = hdrint(2);   % number of 4us samples on gradient plateau
-[dat, rdb_hdr] = toppe.utils.loadpfile(pfile); % dat = [nfid ncoils nslices 1 ny]
-dat = flipdim(dat,1); % as usual
-dat = dat((npre+1):(npre+nro), :, :, :, :);
-dat = permute(dat, [1 5 3 2 4]);  % [nro ny nz ncoils]
+if ~exist('dat', 'var')
+	[dat, rdb_hdr] = toppe.utils.loadpfile(pfile); % dat = [nfid ncoils nslices 1 ny]
+	dat = flipdim(dat,1); % as usual
+	dat = dat((npre+1):(npre+nro), :, :, :, :);
+	dat = permute(dat, [1 5 3 2 4]);  % [nro ny nz ncoils]
+end
 
 [nro ny nz ncoils] = size(dat);
 
@@ -48,10 +57,22 @@ kz = repmat(kz, [1 1 ny]);
 kz = permute(kz, [1 3 2]);
 
 % reduce size (ok since delta_k is 1/4 of design fov)
-dat = dat(2:4:end,:,:,:);
+dats = dat(2:4:end,:,:,:);
 kx = kx(2:4:end,:,:,:);
 ky = ky(2:4:end,:,:,:);
 kz = kz(2:4:end,:,:,:);
+
+% reduce matrix size to speed up this test
+if false
+n = norig/2;
+sensorig = sens;
+sens = zeros(n,n,n,ncoils);
+for ic = 1:ncoils
+	tmp = fftshift(ifftn(fftshift(sensorig(:,:,:,ic))));
+	r = (n/2+1):(3*n/2);
+	sens(:,:,:,ic) = fftshift(fftn(fftshift(tmp(r,r,r))));
+end
+end
 
 nx = ny;   % isotropic resolution and fov
 fov = 25.6; % see getparams.m (in gre3d.tar)
@@ -59,10 +80,18 @@ fov = 25.6; % see getparams.m (in gre3d.tar)
 nufft_args = {[nx,ny,nz],[6,6,6],[2*nx,2*ny,2*nz],[nx/2,ny/2,nz/2],'minmax:kb'};
 mask = true(nx,ny,nz); % Mask for support
 L = 6;
-A = Gmri([fov*kx(:) fov*ky(:) fov*kz(:)], ...
-	mask, 'nufft', nufft_args);
-coil = 20;
-d1 = dat(:,:,:,coil);
-x = reshape(A'*d1(:)/(nx*ny*nz), [nx ny nz]);
+if ~exist('A', 'var')
+	tic
+	A = Gmri([fov*kx(:) fov*ky(:) fov*kz(:)], ...
+		mask, 'nufft', nufft_args);
+	toc
+	A = Asense(A, sens);
+end
 
+x = reshape(A'*dats(:)/(nx*ny*nz), [nx ny nz]);
+
+% do single-coil recon as a check
+%coil = 20;
+%d1 = dats(:,:,:,coil);
+%x = reshape(A'*d1(:)/(nx*ny*nz), [nx ny nz]);
 im(x);
