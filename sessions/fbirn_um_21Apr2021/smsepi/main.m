fb213acc4d86654f645dcf020d770f94e9f098f3
jon <jfnielse@umich.edu>
2021-04-28 15:47:04 -0400

    On branch develop 	modified:   main.m 	new file:   setup.m

--- sessions/fbirn_um_21Apr2021/smsepi/main.m
+++ sessions/fbirn_um_21Apr2021/smsepi/main.m
@@ -1,13 +1,136 @@
+% pfile and frame to reconstruct
+pfile = 'P_smsepi.7';
+pfile = 'P_epi_mb3.7';
+frame = 10; 
+mb = 3;
+
 % sens maps
 load ../gre3d/sens_bart
 sens = sens_bart;
-%sens = flipdim(sens,1); % 0 0 0 1 1 1 1
-%sens = flipdim(sens,2); % 1 1 0 0 1 1 0
-%sens = flipdim(sens,3); % 1 0 1 0 1 0 1
+
+% acquisition info:
+% gpre, gx1 (readout trap), nx, ny, fov
+if 0
+curdir = pwd;
+cd tmp
+addpath ~/github/pulseq/matlab
+smsepi;   % gpre, gx1, gx, gy, nx, ny, fov, etc
+cd(curdir);
+save scanparams.mat gpre gx1 gx nx ny fov kx seq ex
+else
+load scanparams.mat
+end
+
+% SMS slice info
+slSep = ex.sliceSep;
+slThick = seq.slThick;
+
+% caipi pattern
+IZ = caipi(ny,mb,1);
+	
+% data
+[dat, rdb_hdr] = toppe.utils.loadpfile(pfile); % dat = [nfid ncoils nslices 1 ny]
+dat = flipdim(dat,1); % as usual
+dat = dat(:,:,1,1,frame);  % [nfid ncoils]
+[nfid ncoils] = size(dat);
+
+% kspace locations
+[~,gx,gy,gz] = toppe.readmod('readout.mod');  % from smsepi_mb3.tar
+gamma = 4.2576;  % kHz/G
+dt = 4e-3;       % ms
+k = dt*gamma*cumsum([gx gy gz]);
+
+% apply gradient delay
+ph = [-0.14 -1.0];  % constant (rad) and linear (rad/cycle) odd/even phase offset
+k(:,1) = interp1(1:nfid, k(:,1), (1:nfid) + ph(2)/2/(2*pi));
+
+% crop out dephaser gradients before start of EPI train
+ntrap = length(gx1);
+istart = length(gpre) + 3*ntrap + length(gpre) + 1; % see smsepi.m
+istop = istart + ntrap*ny - 1;
+dat = dat(istart:istop,:);
+k = k(istart:istop,:);
+
+dat = reshape(dat, ntrap, ny, ncoils);
+
+% odd/even echo phase offset
+dat(:,1:2:end,:) = exp(+1i*ph(1)/2)*dat(:,1:2:end,:);
+dat(:,2:2:end,:) = exp(-1i*ph(1)/2)*dat(:,2:2:end,:);
+
+% pick out SMS slices from 3D sens map
+isl = slSep/slThick;  % should be integer, see smsepi.m
+nz = size(sens,3);
+IZmb = [nz/2-isl, nz/2, nz/2+isl]; %, nz/2+2*isl];
+sens = sens(:,:,IZmb+1,:);
+
+mb = size(sens,3);
+
+% interpolate onto cartesian grid along readout
+clear dcart;
+kx2d = reshape(k(:,1), ntrap, ny);
+[~,Ao,dcfo] = reconecho([], nx, [], [], kx2d(:,1), fov); % odd echoes
+[~,Ae,dcfe] = reconecho([], nx, [], [], kx2d(:,2), fov); % even echoes
+for ic = 1:ncoils
+	x = zeros(nx,ny);
+	for iy = 1:2:ny
+		x(:,iy) = reconecho(dat(:,iy,ic), nx, Ao, dcfo);
+	end
+	for iy = 2:2:ny
+		x(:,iy) = reconecho(dat(:,iy,ic), nx, Ae, dcfe);
+	end
+	dcart(:,:,ic) = fftshift(fft(fftshift(x,1), [], 1),1);
+end
+dcart(isnan(dcart)) = 0;
+dcart = reshape(dcart, [], ncoils);  % [sum(kmask(:)) ncoils]
+
+% kz encoding and slice offsets
+kzmax = 1/(2*slSep); % cycles/cm
+KZ = (IZ-mb/2-0.5)/(mb/2)*kzmax;
+Z = [(-mb/2+0.5):(mb/2-0.5)]*slSep;  % slice locations (cm)
+
+% reconstruct
+fprintf('Reconstructing...\n');
+imask = true(nx,ny,mb);
+A = Gsms(KZ, Z, sens, imask);
+xinit = zeros(size(imask));
+tol = 1e-6; nitmax = 10;
+tic; [xhat,res] = cgnr_jfn(A, dcart(:), xinit(imask), nitmax, tol); toc;
+xhat = embed(xhat, imask);
+subplot(121); im(xhat)
+subplot(122); plot(res, 'o-');
+
+return;
+
+
+
+
+
+
+
+nufft_args = {[nx,ny,mb],[6,6,6],[2*nx,2*ny,2*mb],[nx/2,ny/2,mb/2],'minmax:kb'};
+mask = true(nx,ny,mb); % Mask for support
+%mask(:,:,mb) = false;
+A0 = Gmri(k, mask, ...
+	'fov', [fov fov slSep*mb], ...
+	'nufft', nufft_args);
+A = Asense(A0, sens);
+
+x0 = reshape(A'*dat(:)/(nx*ny*mb), [nx ny mb]);
+W = 1; C = 0;
+x = qpwls_pcg1(x0, A, W, dat(:), C, ...
+                   'niter', 10);
+%tic; [x,res] = cgnr_jfn(A, dat(:), x0(:), 15, 1e-7); toc; % Also works
+x = reshape(x, [nx ny mb]);
+im(x);
+
+return;
+
+
+
 
 
 %% Synthesize data and recon using 3d sense nufft
-if true
+if false
 
 % data
 pfile = '../epi/P_fmri2depi.7';
@@ -53,7 +176,6 @@ dat = dat(istartdat:istopdat,:,:);
 k = k(istartk:istopk,:);
 
 % simulate SMS acquisition
-% recon each slice using 2d nufft, apply kz encoding, and inufft back
 dat = permute(dat, [1 3 2]);  % [ntrap*ny mb ncoils]
 d2d = reshape(dat, ntrap, ny, mb, []);  % [ntrap ny mb ncoils]
 kzmax = 1/(2*slSep); % cycles/cm
@@ -132,105 +254,6 @@ im(cat(1,x));
 return;
 
 end
-
-%% Recon using 3d sense nufft, following ../gre3d/main.m
-
-% pfile and frame to reconstruct
-pfile = 'P_smsepi.7';
-pfile = 'P_epi_mb3.7';
-frame = 20; 
-
-% sens maps
-load ../gre3d/sens_bart
-sens = sens_bart;
-
-% acquisition info:
-% npre (acquire samples prior to first echo), nx, ny, fov
-if 0
-curdir = pwd;
-cd tmp
-addpath ~/github/pulseq/matlab
-smsepi;   % gpre, gx1, gx, gy, nx, ny, fov, etc
-cd(curdir);
-save scanparams.mat gpre gx1 gx nx ny fov kx seq ex
-else
-load scanparams.mat
-end
-
-% indeces corresponding to EPI train
-ntrap = length(gx1);
-istart = length(gpre) + 3*ntrap + length(gpre) + 1; % see smsepi.m
-istop = istart + ntrap*ny - 1;
-
-% SMS slice info
-slSep = ex.sliceSep;
-slThick = seq.slThick;
-	
-% data
-[dat, rdb_hdr] = toppe.utils.loadpfile(pfile); % dat = [nfid ncoils nslices 1 ny]
-dat = flipdim(dat,1); % as usual
-dat = dat(:,:,1,1,frame);  % [nfid ncoils]
-[nfid ncoils] = size(dat);
-
-% kspace locations
-[~,gx,gy,gz] = toppe.readmod('readout.mod');  % from smsepi_mb3.tar
-gamma = 4.2576;  % kHz/G
-dt = 4e-3;       % ms
-k = dt*gamma*cumsum([gx gy gz]);
-
-% apply gradient delay
-ph = [-0.14 -0.8];  % constant (rad) and linear (rad/cycle) odd/even phase offset
-%k(:,1) = interp1(1:nfid, k(:,1), (1:nfid) + ph(2)/(2*pi));
-
-% crop out dephaser gradients before start of EPI train
-dat = dat(istart:istop,:);
-k = k(istart:istop,:);
-
-d2d = reshape(dat, ntrap, ny, []);
-
-for iy = 1:2:ny
-  % d2d(:,1:2:end,:,isl,:) = exp(+1i*ph(isl,1)/2)*d2d(:,1:2:end,:,isl,:);
-  % d2d(:,2:2:end,:,isl,:) = exp(-1i*ph(isl,1)/2)*d2d(:,2:2:end,:,isl,:);
-end
-
-% odd echoes only (for testing)
-if false
-ysamp = 1:2:ny;
-dat = d2d(:,ysamp,:);
-k = reshape(k, [ntrap ny 3]);
-k = k(:,ysamp,:);
-k = reshape(k, [], 3);
-end
-
-% pick out SMS slices from 3D sens map
-isl = slSep/slThick;  % should be integer, see smsepi.m
-nz = size(sens,3);
-IZmb = [nz/2-isl+1, nz/2+1, nz/2+isl+1]; %, nz/2+2*isl];
-sens = sens(:,:,IZmb,:);
-
-mb = size(sens,3);
-%sens = permute(sens, [1 2 4 3]);  % [nx ny ncoils mb]
-%sens = cat(4, sens, sens(:,:,:,end));
-%sens = permute(sens, [1 2 4 3]);  % [nx ny mb ncoils]
-
-nufft_args = {[nx,ny,mb],[6,6,6],[2*nx,2*ny,2*mb],[nx/2,ny/2,mb/2],'minmax:kb'};
-mask = true(nx,ny,mb); % Mask for support
-%mask(:,:,mb) = false;
-L = 6;
-A0 = Gmri(k, mask, ...
-	'fov', [fov fov slSep*(mb-1)], ...
-	'nufft', nufft_args);
-A = Asense(A0, sens);
-
-x0 = reshape(A'*dat(:)/(nx*ny*mb), [nx ny mb]);
-W = 1; C = 0;
-x = qpwls_pcg1(x0, A, W, dat(:), C, ...
-                   'niter', 10);
-%tic; [x,res] = cgnr_jfn(A, dat(:), x0(:), 15, 1e-7); toc; % Also works
-x = reshape(x, [nx ny mb]);
-im(x);
-
-return;
 
 
 
