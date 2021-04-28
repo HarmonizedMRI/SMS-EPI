4976ef3c6bc658d138451ae274c45b5f2f092fff
jon <jfnielse@umich.edu>
2021-04-28 10:17:18 -0400

    On branch develop 	modified:   Gsms.m

--- recon/Gsms.m
+++ recon/Gsms.m
@@ -19,27 +19,26 @@ arg.imask = imask;
 arg.nc = size(arg.sens,4);
 arg.np = sum(arg.imask(:));    % number of spatial positions (voxels)
 
-arg.nt = arg.nx*arg.ny;  % number of samples per coil
-
 A = fatrix2('arg', arg, ...
 	'idim', [arg.np], ...             % size of x
-	'odim', [arg.nt*arg.nc], ...      % size of A*x
+	'odim', [arg.nx*arg.ny*arg.nc], ...      % size of A*x
 	'forw', @A_forw, 'back', @A_back);
 	%'mask', true(arg.np,1), ...
 
 return
 
 % x  [arg.np] 
-% y  [arg.nt*arg.nc]
+% y  [arg.nx*arg.ny*arg.nc]
 function y = A_forw(arg, x)
 	x = embed(x, arg.imask);  % [nx ny mb]
 	y = zeros(arg.nx, arg.ny, arg.nc);
 	for ic = 1:arg.nc
-		xsum = zeros(arg.nx, arg.ny);
+		tmp = zeros(arg.nx, arg.ny);
 		for iz = 1:arg.mb
-			xsum = xsum + exp(1i*2*pi*arg.KZ(iz)*arg.Z(iz)) * arg.sens(:,:,iz,ic) .* x(:,:,iz);
+			tmp = tmp + exp(1i*2*pi*arg.KZ(iz)*arg.Z(iz)) * ...
+				arg.sens(:,:,iz,ic) .* x(:,:,iz);
 		end
-		y(:,:,ic) = fftshift(fftn(fftshift(xsum)));
+		y(:,:,ic) = fftshift(fftn(fftshift(tmp)));
 	end
 	y = y(:);
 return
@@ -50,7 +49,8 @@ function x = A_back(arg, y)
 	for ic = 1:arg.nc
 		tmp = zeros(arg.nx, arg.ny, arg.mb);
 		for iz = 1:arg.mb
-			tmp(:,:,iz) = exp(-1i*2*pi*arg.KZ(iz)*arg.Z(iz)) * conj(arg.sens(:,:,iz,ic)).*fftshift(ifftn(fftshift(y(:,:,ic))));
+			tmp(:,:,iz) = exp(-1i*2*pi*arg.KZ(iz)*arg.Z(iz)) * ...
+				conj(arg.sens(:,:,iz,ic)) .* fftshift(ifftn(fftshift(y(:,:,ic))));
 		end
 		x = x + tmp;
 	end
