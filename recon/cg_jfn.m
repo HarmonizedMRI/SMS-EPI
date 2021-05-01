dd2830ab1c5f23a2f4d0e2ddd2dcf2b6140644b1
jon <jfnielse@umich.edu>
2021-04-30 20:35:44 -0400

    Doesn't work, don't use On branch develop 	modified:   cg_jfn.m

--- recon/cg_jfn.m
+++ recon/cg_jfn.m
@@ -1,15 +1,14 @@
+function [x,res] = cg_jfn(A,b,x_i,nitmax,tol)
+% function [x,res] = cg_jfn(A,b,x_i,nitmax,tol)
+%
 % from CG without the agonizing pain, p 32
 %
-% $Id: cg_jfn.m,v 1.4 2010-09-23 02:32:47 jfnielse Exp $
-
-function [x] = cg_jfn(A,b,x_i,tol)
   
 r_i = b - A*x_i;
 d_i = r_i;
 
 it = 0;
-nit = 10;
-while norm(r_i) > tol & it < nit
+while norm(r_i) > tol & it < nitmax
 	alpha_i = r_i'*r_i/(d_i'*A*d_i);
 	x_ii = x_i + alpha_i*d_i;
 	r_ii = r_i - alpha_i*A*d_i;
@@ -20,6 +19,8 @@ while norm(r_i) > tol & it < nit
 	d_i = d_ii;
 	x_i = x_ii;
 	it = it + 1;
+
+	res(it) = norm(r_i);
 end
 
 fprintf(1, 'norm(r) = %.e\n', norm(r_i));
