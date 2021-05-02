396fd69a0fd832d2be186507e10e39794adb7c0f
jon <jfnielse@umich.edu>
2021-05-02 08:00:02 -0400

    On branch develop 	modified:   main.m

--- sessions/fbirn_um_21Apr2021/smsepi/main.m
+++ sessions/fbirn_um_21Apr2021/smsepi/main.m
@@ -96,15 +96,19 @@ tmp = sqrt(sum(abs(sens).^2,4)); %true(nx,ny,mb);
 imask = tmp > 0;
 esp = 0.52e-3;  % echo spacing (sec)
 ti = (0:(ny-1))*esp;
-t2 = 50e-3*ones(size(imask));   % sec
+t2 = 50e-3*ones(size(imask));   % sec. T2 of ball phantom is about 50ms
 fmap = 0*ones(size(imask));     % Hz
-A = Gsms(KZ, Z, sens, imask, 'zmap', 1./t2 + 2i*pi*fmap, 'ti', ti); nitmax = 5;
-%A = Gsms(KZ, Z, sens, imask); nitmax = 15;
 xinit = zeros(size(imask));
 tol = 1e-5; 
+
+A = Gsms(KZ, Z, sens, imask); nitmax = 10;
 tic; [xhat,res] = cgnr_jfn(A, dcart(:), xinit(imask), nitmax, tol); toc;
+
+A = Gsms(KZ, Z, sens, imask, 'zmap', 1./t2 + 2i*pi*fmap, 'ti', ti); nitmax = 5;
+tic; [xhat,res] = cgnr_jfn(A, dcart(:), xhat, nitmax, tol); toc;
+
 xhat = embed(xhat, imask);
-subplot(121); im(xhat)
+subplot(121); im(xhat);
 subplot(122); plot(res, 'o-');
 
 return;
