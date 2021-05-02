20143fb37f19e0162152e7d531aa92d6795c6b30
jon <jfnielse@umich.edu>
2021-05-02 08:48:00 -0400

    On branch develop 	modified:   main.m

--- sessions/fbirn_um_21Apr2021/smsepi/main.m
+++ sessions/fbirn_um_21Apr2021/smsepi/main.m
@@ -105,7 +105,7 @@ A = Gsms(KZ, Z, sens, imask); nitmax = 10;
 tic; [xhat,res] = cgnr_jfn(A, dcart(:), xinit(imask), nitmax, tol); toc;
 
 A = Gsms(KZ, Z, sens, imask, 'zmap', 1./t2 + 2i*pi*fmap, 'ti', ti); nitmax = 10;
-tic; [xhat,res] = cgnr_jfn(A, dcart(:), xhat, nitmax, tol); toc;
+%tic; [xhat,res] = cgnr_jfn(A, dcart(:), xhat, nitmax, tol); toc;
 
 xhat = embed(xhat, imask);
 subplot(121); im(xhat);
