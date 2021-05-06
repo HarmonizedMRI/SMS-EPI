8d8f19c9b55cd1d1df7711c6c52add162f391d6a
jon <jfnielse@umich.edu>
2021-05-06 08:59:08 -0400

    On branch develop 	modified:   smsepi.m

--- sequence/smsepi.m
+++ sequence/smsepi.m
@@ -48,72 +48,14 @@ sys.maxSlew = 8;   % G/cm/ms. Reduce PNS during slice rephaser.
 	'ftype', ex.ftype, ...
 	'system', sys);
 
-%% EPI readout
-readouttrap;  % creates gpre and gx1. Also used in fmri2depi.m
+%% EPI readout module
+mxs = system.ge.maxSlew;
+[gx, gy, gz] = getepireadout(fov, nx, ny, mbFactor, ...
+	ex.sliceSep, system.ge.maxGrad, mxs, dt*1e3, fbesp);
 
-% z blip/rewinder. Use one waveform for both and scale as needed.
-kmax = 1/(2*ex.sliceSep);   % cycles/cm
-area = 2*kmax/gamma;        % G/cm * sec
-gzblip = toppe.utils.trapwave2(area, system.ge.maxGrad, system.ge.maxSlew, system.ge.raster*1e3);
-
-% z prewinder
-gzpre = [(mbFactor/2-1/2)/2*gzblip zeros(1,length(gpre)-length(gzblip))];
-gzpre = [gzblip/2 zeros(1,length(gpre)-length(gzblip))];
-
-% gz waveforms for the various echoes
-imax = find(gzblip == max(gzblip));
-gzblipstart = gzblip(1:imax(1));  % first half of blip
-gzblipend = gzblip(imax(2):end);
-amp = 1/(mbFactor/2-1);  % amplitude of one delta_kz step (scale gzblip by amp)
-gz1 = [zeros(1,length(gx1)-length(gzblipstart)) amp*gzblipstart]; % first echo
-gz2 = [amp*gzblipend zeros(1,length(gx1)-length(gzblipend)-length(gzblipstart)) amp*gzblipstart];
-gz3 = [amp*gzblipend zeros(1,length(gx1)-length(gzblipend)-length(gzblipstart)) -gzblipstart];
-gz4 = [-gzblipend zeros(1,length(gx1)-length(gzblipend)-length(gzblipstart)) amp*gzblipstart];
-gzlast = [amp*gzblipend zeros(1,length(gx1)-length(gzblipend))]; 
-
-% put it all together 
-% include 3 calibration echoes at start
-gx = [gpre -gx1 gx1 -gx1];
-gy = 0*gx;
-gz = 0*gx;
-gx = [gx 0*gpre  gx1];
-gy = [gy  -gpre  gy1];
-gz = [gz  -gzpre gz1];
-for iecho = 2:(ny-1)
-	gx = [gx gx1*(-1)^(iecho+1)];
-	gy = [gy gyn];
-end
-gx = [gx gx1*(-1)^(iecho+2) 0];  % add zero at end
-gy = [gy gylast 0];
-for iecho = 2:mbFactor/2:ny
-	gz = [gz repmat(gz2, 1, mbFactor/2-2) gz3 gz4];
-end
-gz = [gz gzlast 0];
-
-% fix gz at end
-gz = gz(1:length(gx));
-gz((end-round(length(gzlast)/2)+1):end) = 0;
-
-% write to readout.mod
-gx = toppe.utils.makeGElength(gx(:));
-gy = toppe.utils.makeGElength(gy(:));
-gz = toppe.utils.makeGElength(gz(:));
 toppe.writemod('gx', gx, 'gy', gy, 'gz', gz, ...
 	'system', system.ge, ...
 	'ofname', 'readout.mod');
-
-% plot kspace
-[~,gx,gy,gz] = toppe.readmod('readout.mod');
-kx = gamma*dt*cumsum(gx);
-ky = gamma*dt*cumsum(gy);
-kz = gamma*dt*cumsum(gz);
-figure;
-subplot(121); plot(kx,ky,'b.'); axis equal;
-xlabel('kx (cycles/cm)'); ylabel('ky (cycles/cm)');
-T = dt*1e3*(1:length(kx));
-subplot(122); hold off; plot(T,kx,'r'); hold on; plot(T,ky,'g'); plot(T,kz,'b'); hold off;
-legend('kx', 'ky', 'kz'); ylabel('cycles/cm'); xlabel('time (ms)');
-
 
 %% Create modules.txt 
 % Entries are tab-separated
@@ -175,9 +117,7 @@ for ifr = 1:nframes
 end
 toppe.write2loop('finish');
 
-tar('smsepi.tar', {'*.txt', '*.mod', '*.m'});
-
-return;
+%tar('smsepi.tar', {'*.txt', '*.mod', '*.m'});
 
 % simulate and plot slice profile
 m0 = [0 0 1];        % initial magnetization
