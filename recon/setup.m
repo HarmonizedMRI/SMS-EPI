6b9833b3174f0c0d3ab4ff0bc6122ae0e743f380
jon <jfnielse@umich.edu>
2021-05-08 12:32:25 -0400

    On branch develop 	modified:   setup.m

--- recon/setup.m
+++ recon/setup.m
@@ -4,6 +4,6 @@ curdir = pwd; cd ~/github/mirt; setup; cd(curdir)        % MIRT
 %% BART setup (to calculate sensitivity maps)
 % Add the following to ~/.bashrc:
 % TOOLBOX_PATH=/home/jon/Programs/bart-0.7.00
-setenv('TOOLBOX_PATH', '/home/jon/Programs/bart-0.7.00'); % if getenv doesn't find it
-addpath(strcat(getenv('TOOLBOX_PATH'), '/matlab'));
+%>> setenv('TOOLBOX_PATH', '/home/jon/Programs/bart-0.7.00'); % if getenv doesn't find it
+%>> addpath(strcat(getenv('TOOLBOX_PATH'), '/matlab'));
 
