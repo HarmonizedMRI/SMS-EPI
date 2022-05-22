c7312aeebcbe3b1cf892ed097bc2d0cb44dba854
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2022-05-22 16:58:54 -0400

    Use the Stirnberg skippedcaipi toolbox On branch develop 	modified:   getcaipiepireadout.m

--- sequence/GE/getcaipiepireadout.m
+++ sequence/GE/getcaipiepireadout.m
@@ -1,23 +1,24 @@
-function [gx, gy, gz, gpre, esp, gx1, kz, nBlipMax] = getcaipiepireadout(fov, imSize, Ry, pf_ky, Rz, CaipiShiftZ, raster, varargin) 
-% SegmentationFactor, gMax, slewRead, slewPre, raster, fbesp)
-%function [gx, gy, gz, gpre, esp, gx1, kz, nBlipMax] = getcaipiepireadout(fov, imSize, Ry, pf_ky, Rz, Delta, gMax, slewRead, slewPre, raster, fbesp)
+function [gx, gy, gz, gpre, esp, gx1, kz, nBlipMax] = getcaipiepireadout(FOV, imSize, Ry, pf_ky, Rz, CaipiShiftZ, varargin) 
+%function [gx, gy, gz, gpre, esp, gx1, kz, nBlipMax] = getcaipiepireadout(FOV, imSize, Ry, pf_ky, Rz, CaipiShiftZ, gMax, slewRead, slewPre, raster, fbesp)
 %
 % Created 3D EPI CAIPI readout waveform.
 % For now, assumes isotropic resolution.
-% To generate single-echo GRE readout, use Ry = imSize(2)
+% Assumes gradient raster = ADC dwell time = 4us (for now).
 %
 % Inputs:
-%  fov       [1 3]  cm
-%  imSize    [1 3]  image volume matrix size
-%  Ry        [1]    ky acceleration factor (can be non-integer)
-%  Rz        [1]    kz acceleration factor (integer)
-%  Delta     [1]    size of kz step (integer multiples of 1/fov(3))). Must be > 0.
+%  FOV       [1 3]  cm
+%  imSize    [1 3]  image matrix size
+%  Ry        [1]    ky acceleration factor
+%  Rz        [1]    kz acceleration factor
 %  pf_ky     [1]    Partial Fourier factor (along ky). 
-%  gMax      Gauss/cm
+%  CaipiShiftZ  [1]    size of kz step (integer multiples of 1/FOV(3))). Must be > 0.
+% 
+% Optional keyword arguments
+%  gMax      [1]    Gauss/cm
 %  slewRead  [1 3]  max slew along x/y/z gradient axis (Gauss/cm/ms)
 %  slewPre   [1] max slew during prephasing gradient trapezoid (Gauss/cm/ms)
-%  raster    ms
 %  fbesp     [2] forbidden echo spacing range (ms)
+%  plot      boolean    plot k-space? Default: false
 %
 % Outputs:
 %  gx     x readout waveform (G/cm), without prephaser at beginning
@@ -27,65 +28,72 @@ function [gx, gy, gz, gpre, esp, gx1, kz, nBlipMax] = getcaipiepireadout(fov, im
 %  esp    echo spacing (ms)
 %  gx1    waveform for one echo (G/cm)
 %  kz     kz encoding indeces
+%
+%  See caipiepifmri.m for how to use these outputs to construct a TOPPE fMRI scan.
 
-arg.SegmentationFactor = 1;
+if strcmp(FOV, 'test')
+    sub_test();
+    return
+end
+
+if CaipiShiftZ < 1 | rem(CaipiShiftZ,1)
+    error('CaipiShiftZ must be non-negative integer');
+end
+
+% Set keyword argument defaults and parse input
 arg.gMax = 20;      % Peak gradient amplitude (Gauss/cm)
-arg.slewRead = 10;  % Limit slew rate to this value (Gauss/cm/ms), to control PNS.
+arg.slewRead = [15 15 15];  % Limit slew rate to this value (Gauss/cm/ms), to control PNS.
+arg.slewPre = 12;   % Limit slew rate to this value (Gauss/cm/ms) during prewinder.
+arg.fbesp = [0.41 0.51]; % forbidden echo spacing range (ms)
+arg.plot = false;
+arg.SegmentationFactor = 1;
 
 arg = toppe.utils.vararg_pair(arg, varargin);
 
-
-Delta = CaipiShiftZ;
-
-% check inputs
-if mod(Delta,1) | Delta < 1
-    error('Delta must be positive integer');
-end
-
-if mod(Rz, 1)
-    error('Rz must be a positive integer')
-end
-
-if pf_ky < 0.5 | pf_ky > 1.0
-    error('Partial Fourier factor must be in the range [0.5 1.0]');
+if pf_ky < 0.7 | pf_ky > 1.0
+    error('Partial Fourier factor must be in the range [0.7 1.0]');
 end
 
 nx = imSize(1); ny = imSize(2); nz = imSize(3);
 
-dt = raster;
+dt = 4e-3;   % ms
 gamma = 4257.6;        % Hz/G
 
-res = fov(1)/nx;       % spatial resolution (cm)
+res = FOV(1)/nx;       % spatial resolution (cm)
 kmax = 1/(2*res);      % cycles/cm
 area = 2*kmax/gamma;   % G/cm * sec (area of each readout trapezoid)
-dkx = 1/fov(1);
+dkx = 1/FOV(1);
 
-dky = Ry/fov(2);       % ky spacing (cycles/cm)
-dkz = 1/fov(3);        % kz spacing (cycles/cm) corresponding to Delta=1
+dky = Ry/FOV(2);       % ky spacing (cycles/cm)
+dkz = 1/FOV(3);        % kz spacing (cycles/cm) corresponding to CaipiShiftZ=1
 
 etl = ceil(pf_ky*ny/Ry);
 
 % kz-encode indeces for one echo train
-kz = [];
-for ii = 1:Delta
-    kz = [kz ii:Delta:Rz];
-end
-kz = repmat(kz, [1 ceil(etl/Rz)]);
-kz = kz(1:etl);
+system(sprintf('python skippedcaipi_sampling.py %d %d %d %d %d %d', ...
+    Ry, Rz, CaipiShiftZ, 1, ny, nz));
+load caipi
+kz1 = double(indices(:,1)' + 1);
+%kz = [];
+%for ii = 1:CaipiShiftZ
+%    kz = [kz ii:CaipiShiftZ:Rz];
+%end
+%kz = repmat(kz, [1 ceil(etl/Rz)]);
+kz = kz1(1:etl);
 
 % kz encoding blip amplitudes (multiples of dkz)
-kzAmp = [diff(kz)];
+kzAmp = diff(kz);
 
 % phase-encode blips
-gyBlip = toppe.utils.trapwave2(dky/gamma, gMax, slewRead(2), dt);  % PE1
+gyBlip = toppe.utils.trapwave2(dky/gamma, arg.gMax, arg.slewRead(2), dt);  % PE1
 nyBlip = length(gyBlip);
-gzBlip = toppe.utils.trapwave2(dkz*max(abs(kzAmp))/gamma, gMax, slewRead(3), dt);  % PE2
+gzBlip = toppe.utils.trapwave2(dkz*max(abs(kzAmp))/gamma, arg.gMax, arg.slewRead(3), dt);  % PE2
 nzBlip = length(gzBlip);
-nBlipMax = max([nyBlip nzBlip]);
+nBlipMax = max([nyBlip nzBlip]); % blip duration
 
 % readout trapezoid (with ramp sampling)
-mxg = min(1/(fov(1)*gamma*dt*1e-3), gMax);    % Gauss/cm
-gx1 = toppe.utils.trapwave2(area, mxg, slewRead(1), dt);
+mxg = min(1/(FOV(1)*gamma*dt*1e-3), arg.gMax);    % Gauss/cm
+gx1 = toppe.utils.trapwave2(area, mxg, arg.slewRead(1), dt);
 
 % Extend readout plateau to make room for turns
 gx1orig = gx1;
@@ -99,11 +107,11 @@ end
 
 % Reduce peak gradient until echo spacing is outside forbidden range
 esp = length(gx1)*dt;  % echo spacing (ms)
-if esp > fbesp(1) & esp < fbesp(2)
+if esp > arg.fbesp(1) & esp < arg.fbesp(2)
     for s = 1:-0.005:0.1
         mxg = s*mxg;
-        gx1 = toppe.utils.trapwave2(area, mxg, slewRead(1), dt);
-        if length(gx1)*dt > fbesp(2)
+        gx1 = toppe.utils.trapwave2(area, mxg, arg.slewRead(1), dt);
+        if length(gx1)*dt > arg.fbesp(2)
             esp = length(gx1)*dt;
             break;
         end
@@ -140,28 +148,44 @@ gy = toppe.makeGElength(gy(:));
 gz = toppe.makeGElength(gz(:));
 
 % x/y/z prephasers.
-% x prephaser a bit bigger since area of gx1 is a bit larger than 'area'
 areax = sum(gx1)*dt*1e-3;   % G/cm * sec
-gpre.x = toppe.utils.trapwave2(areax/2, gMax, slewPre, dt);
-gpre.y = toppe.utils.trapwave2(area/2-dky/Ry/gamma/2, gMax, slewPre, dt);
+gpre.x = toppe.utils.trapwave2(areax/2, arg.gMax, arg.slewPre, dt);
+gpre.y = toppe.utils.trapwave2(area/2-dky/Ry/gamma/2, arg.gMax, arg.slewPre, dt);
 gpre.y = [gpre.y(:); zeros(length(gpre.x)-length(gpre.y), 1)]; % make same length
 gpre.x = toppe.makeGElength(gpre.x(:)); % make length multiple of 4
 gpre.y = toppe.makeGElength(gpre.y(:));
 gpre.z = gpre.y; % isotropic resolution
 
-% plot k-space. Add prephaser for plotting purposes only.
-figure;
-gxf = [-gpre.x; gx];
-gyf = [-gpre.y; gy];
-gzf = [-gpre.y; gz];
-kxp = gamma*dt*1e-3*cumsum(gxf);
-kyp = gamma*dt*1e-3*cumsum(gyf);
-kzp = gamma*dt*1e-3*cumsum(gzf);
-subplot(121); plot(kxp,kyp,'b.'); axis equal;
-xlabel('kx (cycles/cm)'); ylabel('ky (cycles/cm)');
-T = dt*(1:length(kxp));
-subplot(122); hold off; plot(T,kxp,'r'); hold on; plot(T,kyp,'g'); plot(T,kzp,'b'); hold off;
-legend('kx', 'ky', 'kz'); ylabel('cycles/cm'); xlabel('time (ms)');
+if arg.plot
+    % plot k-space. Add prephaser for plotting purposes only.
+    figure;
+    gxf = [-gpre.x; gx];
+    gyf = [-gpre.y; gy];
+    gzf = [-gpre.z; gz];
+    kxp = gamma*dt*1e-3*cumsum(gxf);
+    kyp = gamma*dt*1e-3*cumsum(gyf);
+    kzp = gamma*dt*1e-3*cumsum(gzf);
+    subplot(121); plot(kxp,kyp,'b.'); axis equal;
+    xlabel('kx (cycles/cm)'); ylabel('ky (cycles/cm)');
+    T = dt*(1:length(kxp));
+    subplot(122); hold off; plot(T,kxp,'r'); hold on; plot(T,kyp,'g'); plot(T,kzp,'b'); hold off;
+    legend('kx', 'ky', 'kz'); ylabel('cycles/cm'); xlabel('time (ms)');
+end
 
 return
 
+
+function sub_test()
+
+    res = 0.275;  % cm
+    imSize = [80 80 60];
+    FOV = imSize*res;  % cm
+    Ry = 1;
+    pf_ky = 0.8;
+    Rz = 6;
+    CaipiShiftZ = 2;
+
+    [gx, gy, gz, gpre, esp, gx1, kz, nBlipMax] = getcaipiepireadout(FOV, imSize, Ry, pf_ky, Rz, CaipiShiftZ, ...
+        'plot', true) ;
+
+return
