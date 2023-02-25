9db88d567bb00e49a30b61befe7e92bbb7ecd054
StevenWhitaker <steventwhitaker@gmail.com>
2023-02-24 19:13:25 -0700

    Add `slices` option for `getsense`.

--- recon/GE/getsense.m
+++ recon/GE/getsense.m
@@ -10,6 +10,12 @@
 % readoutFile  File location of the TOPPE .mod file used for the readout portion
 %              of the SPGR scan
 %                Default: 'readout.mod'
+% slices       Array of slice indices to keep; for example, the SPGR scans used
+%              for sensitivity mapping might include a few more slices above
+%              and below those acquired during the SMS-EPI scan to avoid slab
+%              profile effects and the resulting negative impact on the
+%              computed sensitivity maps
+%                Default: 1:size(spgr, 3) (keep all slices)
 % outfile      String specifying where to save the sensitivity maps;
 %              set to '' to skip saving the output to disk
 %                Default: 'sense.mat'
@@ -27,7 +33,10 @@ function [smap, spgr] = getsense(varargin)
 
     arg.pfile = 'P,b0.7';
     arg.readoutFile = 'readout.mod';
+    arg.slices = [];
     arg.outfile = 'sense.mat';
+
+    arg = toppe.utils.vararg_pair(arg, varargin);
 
     % Load data from SPGR scan
     [~, spgr, dsense] = toppe.utils.recon3dft(arg.pfile, ...
@@ -40,6 +49,12 @@ function [smap, spgr] = getsense(varargin)
     fprintf('Computing sensitivity maps (this may take a while)\n');
     tic; sense = bart('ecalib -r 20', dsense); toc;
     smap = bart('slice 4 0', sense); % Equivalently: smap = squeeze(sense(:,:,:,:,1));
+
+    % Remove unneeded slices
+    if ~isempty(arg.slices)
+        smap = smap(:,:,arg.slices,:);
+        spgr = spgr(:,:,arg.slices);
+    end
 
     % Save results
     if ~isempty(arg.outfile)
