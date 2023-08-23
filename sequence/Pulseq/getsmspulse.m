42b39086dd3c423f6da723c2392c52ceda887df8
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2023-08-23 12:24:29 -0400

    On branch develop 	modified:   getsmspulse.m

--- sequence/Pulseq/getsmspulse.m
+++ sequence/Pulseq/getsmspulse.m
@@ -9,7 +9,7 @@ function [rf, gzRF, freq] = getsmspulse(alpha, slThick, tbw, dur, nSlices, slice
 %   tbw          time-bandwidth product
 %   dur          pulse duration (sec)
 %   nSlices      Multi-band factor
-%   sliceSep     Center-to-center slice separation (cm)
+%   sliceSep     Center-to-center slice separation (m)
 %   sysGE        system struct for TOPPE, see toppe.systemspecs()
 %   sys          Pulseq system struct, see mr.opts()
 %
@@ -35,8 +35,9 @@ arg.type         = 'ex';               % 'ex': 90 excitation; 'st' = small-tip
 arg.ftype        = 'ls';
 arg = toppe.utils.vararg_pair(arg, varargin);
 
+%% Design rf and gradient (on 4us raster)
 % design the 'unit' (base) pulse
-nSpoilCycles = 1e-3;   % just has to be small enough so that no spoiler is added at beginning of waveform in makeslr()
+nSpoilCycles = 1e-6;   % just has to be small enough so that no spoiler is added at beginning of waveform in makeslr()
 [rf1,g] = toppe.utils.rf.makeslr(alpha, slThick, tbw, dur, nSpoilCycles, sysGE, ...
 	'type', arg.type, ...   
 	'ftype', arg.ftype, ...  
@@ -49,24 +50,20 @@ iPeak = mean(I) + 2;
 
 % Create SMS pulse
 PHS = getsmsphase(nSlices);  % Phase of the various subpulses (rad). From Wong et al, ISMRM 2012 p2209.
-bw = tbw/dur*1e3;          % pulse bandwidth (Hz)
-gPlateau = max(g);       % gradient amplitude during RF excitation (Gauss/cm)
+bw = tbw/dur*1e3;            % pulse bandwidth (Hz)
+gPlateau = max(g);           % gradient amplitude during RF excitation (Gauss/cm)
 rf = 0*rf1;
-dt = sysGE.raster;           % sample (dwell) time (sec) 
+dt = sysGE.raster*1e-6;      % sample (dwell) time (sec) 
 t = [dt:dt:(dt*length(rf1))]' - (dt*iPeak);
 for sl = 1:nSlices
 	sliceOffset = (-nSlices/2 + 0.5 + sl-1) * sliceSep;   % cm
-	f = sysGE.gamma*gPlateau*sliceOffset;   % Hz
+	f = sysGE.gamma*gPlateau*1e-4*sliceOffset;   % Hz
 	rf = rf + rf1.*exp(1i*2*pi*f*t)*exp(1i*PHS(sl));
 end
 
-freq = sysGE.gamma*gPlateau*sliceSep;   % Hz
+freq = sysGE.gamma*gPlateau*1e-4*sliceSep;   % Hz
 
-% pad to 4-sample boundary
-%rf = toppe.makeGElength(rf);   
-%g = toppe.makeGElength(g);
-
-% simulate and plot slice profile
+%% Simulate and plot slice profile
 if arg.doSim
 	fov = 20;            % cm
 	m0 = [0 0 1];        % initial magnetization
@@ -97,16 +94,6 @@ if arg.doSim
 
 end
 
-% write to TOPPE .mod file
-if arg.writeModFile
-	% wrap waveforms in toppe.utils.makeGElength() to make sure they are on a 4-sample (16 us) boundary
-	toppe.writemod(sysGE, 'rf', toppe.utils.makeGElength(rf), ...
-		'gz', toppe.utils.makeGElength(g), ...
-		'ofname', arg.ofname);
-
-	% Plot .mod file. Note PNS waveform (can easily exceed 80%/100% threshold).
-	%toppe.plotmod('tipdown.mod');
-end
 
 %% Create Pulseq objects
 
@@ -122,7 +109,6 @@ grad.first = grad.waveform(1);
 grad.last = grad.waveform(end);
 grad.tt = (1:length(grad.waveform)) * rasterIn - rasterIn/2;
 [g, tt] = gradinterp(grad, rasterIn, rasterOut);
-keyboard
 
 % trim zeros at start/end of RF pulse
 % delay (s) must be on grad raster boundary
@@ -135,8 +121,8 @@ wav = wav(1:(end-I(1)+1));
 
 % zero-pad RF waveform duration to gradient raster boundary
 wavdur = length(wav)*sys.rfRasterTime;
-ttarget = pulsegeq.roundtoraster(wavdur, sys.gradRasterTime);
-wav = [wav zeros(1, round((ttarget-wavdur)/sys.rfRasterTime))];
+ttarget = ceil(wavdur/sys.gradRasterTime)*sys.gradRasterTime;
+wav = [wav(:); zeros(round((ttarget-wavdur)/sys.rfRasterTime), 1)];
 
 % if delay < sys.rfDeadTime, set to rfDeadTime and delay gradients accordingly
 gdelay = sys.rfDeadTime - delay;
@@ -148,18 +134,23 @@ rf = mr.makeArbitraryRf(wav, alpha/180*pi, ...
             'system', sys);
 gzRF = mr.makeArbitraryGrad('z', g, sys, ...
     'delay', gdelay);
+gzRF.first = grad.first;
 
 return
 
+
 function [rf, gzRF] = sub_test
-alpha = 90;        % degrees
+alpha = 70;        % degrees
 slThick = 5e-3;    % m
 sliceSep = 40e-3;  % m
 tbw = 6;
-dur = 4e-3;        % s
+dur = 8e-3;        % s
 mb = 5;            % multiband factor (number of slices)
 sysGE = toppe.systemspecs();
-sys = mr.opts();
+sys = mr.opts('rfDeadTime', 100e-6, ...
+    'rfRingdownTime', 60e-6, ...
+    'adcRasterTime', 2e-6, ...
+    'adcDeadTime', 40e-6);
 [rf, gzRF] = getsmspulse(alpha, slThick, tbw, dur, mb, sliceSep, sysGE, sys, ...
 	'doSim', true);
 
