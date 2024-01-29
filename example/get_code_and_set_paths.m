32d5195ea6c5234a38d5d454b9bb232f1b1ba8bc
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2024-01-28 21:23:14 -0500

    Use custom .h5 file format
    On branch develop
    	modified:   get_acs_data.m
    	modified:   get_code_and_set_paths.m
    	modified:   get_ghost_calibration_data.m
    	modified:   recon_timeseries.m
    	modified:   set_experimental_params.m

--- example/get_code_and_set_paths.m
+++ example/get_code_and_set_paths.m
@@ -1,4 +1,7 @@
 %% Get MATLAB toolboxes
+ 
+% toolbox for loading GE raw files
+addpath ~/Programs/orchestra-sdk-2.1-1.matlab/
 
 % Pulseq (+mr namespace)
 system('git clone git@github.com:pulseq/pulseq.git');
@@ -17,4 +20,3 @@ addpath toppe
 % 'im' function for display
 system('git clone git@github.com:JeffFessler/mirt.git');
 cd mirt; setup; cd ..;
- 
--- example/get_ghost_calibration_data.m
+++ example/get_ghost_calibration_data.m
@@ -2,13 +2,14 @@
 %% Get odd/even echo correction parameters 
 
 % load raw data and interpolate to Cartesian grid
-fn = [datdir pfile_cal]; 
-draw = hmriutils.epi.loadframeraw_ge(fn, etl, np, 1);   % [nFID etl np nc]
+fn = [datdir datfile_ghostcal '.h5'];
+%draw = hmriutils.epi.loadframeraw_ge(fn, etl, np, 1);   % [nFID etl np nc]
+draw = hmriutils.epi.io.readframe(fn, 1);
 draw = squeeze(draw(:,:, round(np/2), :));  % we only need one shot
 dc = hmriutils.epi.rampsampepi2cart(draw, kxo, kxe, nx, fov(1)*100, 'spline');
 
 % estimate linear odd/even phase offset
 x = fftshift(ifft(fftshift(dc), [], 1));  % getoephase expects image space
-verbose = false;
+verbose = true;
 a = hmriutils.epi.getoephase(x, verbose); % 'a' contains constant and linear odd/even phase offsets
 
