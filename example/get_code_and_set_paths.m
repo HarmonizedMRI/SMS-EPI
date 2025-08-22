66a8d01e0034491639a459eb2b3e969cc0415882
Jon-Fredrik Nielsen <jfnielsen@gmail.com>
2025-08-22 18:06:56 -0400

    On branch example 	new file:   get_code_and_set_paths.m 	modified:   recon_example.m 	new file:   sessions/sub00004-umich-uhp-20250717-1/pulseq/scans.txt 	new file:   set_experimental_parameters.m

---
+++ example/get_code_and_set_paths.m
@@ -0,0 +1,28 @@
+%% Get MATLAB toolboxes
+
+% Pulseq (+mr namespace)
+%system('git clone git@github.com:pulseq/pulseq.git');
+%addpath pulseq/matlab
+
+addpath ./lib/
+addpath ./lib/
+addpath ~/github/jfnielsen/scanLog/SiemensRead/   % Read Siemens .dat files
+addpath ~/Programs/orchestra-sdk-2.1-1.matlab/   % toolbox for loading GE raw files
+
+% hmriutils toolbox
+system('git clone --branch develop git@github.com:HarmonizedMRI/utils.git');
+addpath utils
+ 
+% +toppe toolbox (misc GE-related scripts)
+%system('git clone --branch develop git@github.com:HarmonizedMRI/PulCeq.git');
+%addpath PulCeq/matlab
+system('git clone --branch develop git@github.com:toppeMRI/toppe.git');
+addpath toppe
+
+% 'im' function for display
+system('git clone git@github.com:JeffFessler/mirt.git');
+cd mirt; 
+setup; 
+%ir_mex_build_mri;
+%ir_mex_build_table;
+cd ..;
