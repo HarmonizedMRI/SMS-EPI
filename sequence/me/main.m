16ae58e8aeb8314ecb30f1c9644e8797fb5750ff
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2025-12-05 23:58:32 -0500

    On branch develop 	modified:   main.m

--- sequence/me/main.m
+++ sequence/me/main.m
@@ -1,20 +1,19 @@
 % Create multi-echo SMS-EPI sequence files.
-% 1. ME SMS-EPI fMRI sequence. Defines readout (gro) and adc events
-% 2. noise scan (same as 1. except no rf)
+% 1. ME SMS-EPI fMRI sequence
+% 2. noise scan (same as #1 except no rf)
 % 3. EPI ghosting and receive gain calibration scan (blips off)
 % 4. mb=1, Ry=2, ME slice grappa calibration scan
 % 5. mb=1, Ry=1 grappa calibration scan
-% 6. 3D GRE B0 mapping (can also be used for sens maps and/or grappa calibration)
+% 6. 3D GRE B0 mapping (also usable for sensitivity maps or grappa calibration)
 
 TODO = [1 1 1 1 1 0];
 TODO = [1 0 0 0 0 0];
 TODO = ones(1,6);
 
-
 %----------------------------------------------------------
 % Sequence parameters
 %----------------------------------------------------------
-% e.g., SMS=4, Ry=2, pf_ky = 0.85, fov 24x24cm, 80x80, 3mm iso:
+% SMS=4, Ry=2, pf_ky = 0.85, fov 24x24cm, 80x80, 3mm iso:
 % Cohen, Alexander D., et al. "Detecting task functional MRI activation 
 % using the multiband multiecho (MBME) echo‐planar imaging (EPI) sequence." 
 % Journal of Magnetic Resonance Imaging 53.5 (2021): 1366-1374.
@@ -26,7 +25,7 @@ voxelSize = [3 3 3] * 1e-3;   % m
 alpha = 60;
 pf_ky = 0.85; %(nx-3*6)/nx;
 
-TR = 0.1*11;   % volume TR (sec)
+TR = 0.9;  %0.1*11;   % volume TR (sec)
 
 mb = 4; Ry = 2; caipiShiftZ = 2;
 [IY, IZ] = getcaipi(ny, nz, Ry, mb, caipiShiftZ, '3DEPI/caipi');
@@ -37,9 +36,8 @@ nTE = 3;
 IY = repmat(IY, nTE, 1);
 IZ = repmat(IZ, nTE, 1);
 
-
 %----------------------------------------------------------
-% scanner settings
+% scanner hardware settings
 %----------------------------------------------------------
 vendor = 'GE';   % 'GE' or 'Siemens'
 
@@ -58,7 +56,7 @@ switch lower(vendor)
         % Settings for Siemens Vida bay MR7 @ U-Mich University Hospital
         gySign = 1;
         freqSign = -1;
-        fatFreqSign = -1;  % Yes, confirmed on Prisma Fit at VA Tech 2024-Oct-24, and Urbana-Champaign
+        fatFreqSign = -1;  % confirmed on Prisma Fit at VA Tech 2024-Oct-24, and Urbana-Champaign
         doConj = false;
         gradRasterTime = 10e-6;
         rfRasterTime = 4e-6;
@@ -78,7 +76,7 @@ sys = mr.opts('maxGrad', 40, 'gradUnit','mT/m', ...
               'B0', B0);
 
 %----------------------------------------------------------
-% input options for writeEPI.m
+% Options passed to writeEPI.m
 %----------------------------------------------------------
 opts = struct('fatSat', fatSat, ...
     'RFspoil', RFspoil, ...
@@ -90,7 +88,7 @@ opts = struct('fatSat', fatSat, ...
     'trigOut', false, ...
     'doNoiseScan', false, ...
     'plot', true, ...
-    'simulateSliceProfile', true);
+    'simulateSliceProfile', false);
 
 %----------------------------------------------------------
 % write sequences
@@ -99,40 +97,47 @@ opts = struct('fatSat', fatSat, ...
 % fmri.seq 
 nFrames = 1;
 if TODO(1)
-    writeEPI('fmri', sys, voxelSize, [nx ny nz], TR, alpha, mb, IY, IZ, nFrames, 'SMS', opts);
+    writeEPI('fmri', sys, voxelSize, [nx ny nz], TR, alpha, mb, IY, IZ, ...
+        nFrames, 'SMS', opts);
 end
 
 % noise.seq
 if TODO(2)
-    writeEPI('noise', sys, voxelSize, [nx ny nz], TR, alpha, mb, IY, IZ, nFrames, 'SMS', ...
+    writeEPI('noise', sys, voxelSize, [nx ny nz], TR, alpha, mb, IY, IZ, ...
+        nFrames, 'SMS', ...
         pge2.utils.setfields(opts, 'doNoiseScan', true));
 end
 
 % epical.seq
 %  - EPI ghost calibration
 if TODO(3)
-    writeEPI('epical', sys, voxelSize, [nx ny nz], TR*mb, alpha, 1, IY, IZ, nFrames, 'SMS', ...
+    writeEPI('epical', sys, voxelSize, [nx ny nz], TR*mb, alpha, 1, IY, IZ, ...
+        nFrames, 'SMS', ...
         pge2.utils.setfields(opts, 'doRefScan', true));
 end
 
 % slgcal.seq
 %  - slice GRAPPA calibration
 if TODO(4)
-    writeEPI('slgcal', sys, voxelSize, [nx ny nz], TR*mb, alpha, 1, IY, ones(size(IZ)), nFrames, 'SMS', opts);
+    writeEPI('slgcal', sys, voxelSize, [nx ny nz], TR*mb, alpha, 1, IY, ones(size(IZ)), ...
+        nFrames, 'SMS', opts);
 end
 
 % grappacal.seq
 %  - GRAPPA calibration
 if TODO(5)
     [IYtmp, IZtmp] = getcaipi(ny, nz, 1, 1, 0, '3DEPI/caipi');
-    writeEPI('grappacal', sys, voxelSize, [nx ny nz], TR*mb, alpha, 1, IYtmp, IZtmp, nFrames, 'SMS', opts);
+    writeEPI('grappacal', sys, voxelSize, [nx ny nz], TR*mb, alpha, 1, IYtmp, IZtmp, ...
+        nFrames, 'SMS', opts);
 end
 
 % b0.seq
 %  - 3D B0 field / coil sensitivity maps
 if TODO(6)
     writeB0('b0', ...
-        pge2.utils.setfields(sys, 'maxSlew', sys.maxSlew*0.5, 'maxGrad', sys.maxGrad*0.5), ...
+        pge2.utils.setfields(sys, ...
+            'maxSlew', sys.maxSlew*0.5, ...
+            'maxGrad', sys.maxGrad*0.5), ...
         voxelSize, [nx nx nx], 4);
 end
 
