2d9abadac39b9467957bfe79486507b335238675
Jon-Fredrik Nielsen <40243850+jfnielsen@users.noreply.github.com>
2025-12-06 20:48:03 -0500

    Add multi-echo SMS-EPI sequence (#9)
    * New function creates SMS-EPI sequence with arbitrary ky/kz encoding schedules:
       me/writeEPI.m
    ---------
    
    Co-authored-by: Jon-Fredrik Nielsen <jfnielse@umich.edu>

---
+++ sequence/me/main.m
@@ -0,0 +1,178 @@
+% Create multi-echo SMS-EPI sequence files.
+% 1. ME SMS-EPI fMRI sequence
+% 2. noise scan (same as #1 except no rf)
+% 3. EPI ghosting and receive gain calibration scan (blips off)
+% 4. mb=1, Ry=2, ME slice grappa calibration scan
+% 5. mb=1, Ry=1 grappa calibration scan
+% 6. 3D GRE B0 mapping (also usable for sensitivity maps or grappa calibration)
+
+TODO = [1 0 0 0 0 0];
+TODO = ones(1,6);
+
+%----------------------------------------------------------
+% sequence parameters
+%----------------------------------------------------------
+% SMS=4, Ry=2, pf_ky = 0.85, fov 24x24cm, 80x80, 3mm iso:
+% Cohen, Alexander D., et al. "Detecting task functional MRI activation 
+% using the multiband multiecho (MBME) echo‐planar imaging (EPI) sequence." 
+% Journal of Magnetic Resonance Imaging 53.5 (2021): 1366-1374.
+
+fatSat = true;
+RFspoil = true;
+nx = 80; ny = nx; nz = 44;
+voxelSize = [3 3 3] * 1e-3;   % m
+alpha = 60;
+pf_ky = 0.85; %(nx-3*6)/nx;
+
+TR = 0.9;  %0.1*11;   % volume TR (sec)
+
+PNSwt = [0.8 1 0.7];   % PNS direction/channel weights
+
+mb = 4; Ry = 2; caipiShiftZ = 2;
+[IY, IZ] = getcaipi(ny, nz, Ry, mb, caipiShiftZ, '3DEPI/caipi');
+etl = 2*ceil(pf_ky*ny/Ry/2);  % echo train length for one TE 
+IY = IY(end-etl+1:end);
+IZ = IZ(end-etl+1:end);
+nTE = 3;   
+IY = repmat(IY, nTE, 1);
+IZ = repmat(IZ, nTE, 1);
+
+%----------------------------------------------------------
+% scanner hardware settings
+%----------------------------------------------------------
+vendor = 'GE';   % 'GE' or 'Siemens'
+
+switch lower(vendor)
+    case 'ge'
+        % Settings for GE scanners at U-Mich
+        gySign = 1;
+        freqSign = 1;
+        fatFreqSign = -1;
+        doConj = false;
+        gradRasterTime = 4e-6;
+        rfRasterTime = 4e-6;
+        blockDurationRaster = 4e-6;
+        B0 = 3;
+
+        psd_rf_wait = 100e-6;  % RF-gradient delay, scanner specific (s)
+        psd_grd_wait = 100e-6; % ADC-gradient delay, scanner specific (s)
+        b1_max = 0.25;         % Gauss
+        g_max = 5;             % Gauss/cm
+        slew_max = 20;         % Gauss/cm/ms
+        coil = 'xrm';          % 'hrmbuhp' (UHP); 'xrm' (MR750)
+        sysGE = pge2.opts(psd_rf_wait, psd_grd_wait, b1_max, g_max, slew_max, coil);
+
+    case 'siemens'
+        % Settings for Siemens Vida bay MR7 @ U-Mich University Hospital
+        gySign = 1;
+        freqSign = -1;
+        fatFreqSign = -1;  % confirmed on Prisma Fit at VA Tech 2024-Oct-24, and Urbana-Champaign
+        doConj = false;
+        gradRasterTime = 10e-6;
+        rfRasterTime = 1e-6;
+        blockDurationRaster = 10e-6;
+        B0 = 2.89;
+end
+
+sys = mr.opts('maxGrad', 40, 'gradUnit','mT/m', ...
+              'maxSlew', 150, 'slewUnit', 'T/m/s', ...
+              'rfDeadTime', 100e-6, ...
+              'rfRingdownTime', 60e-6, ...
+              'adcDeadTime', 0e-6, ...
+              'adcRasterTime', 4e-6, ...
+              'gradRasterTime', gradRasterTime, ...
+              'rfRasterTime', rfRasterTime, ...
+              'blockDurationRaster', blockDurationRaster, ...
+              'B0', B0);
+
+%----------------------------------------------------------
+% options passed to writeEPI.m
+%----------------------------------------------------------
+opts = struct('fatSat', fatSat, ...
+    'RFspoil', RFspoil, ...
+    'gySign', gySign, ...
+    'freqSign', freqSign, ...
+    'fatFreqSign', fatFreqSign, ...
+    'doConj', doConj, ...
+    'doRefScan', false, ...
+    'trigOut', false, ...
+    'doNoiseScan', false, ...
+    'plot', false, ...
+    'simulateSliceProfile', false);
+
+%----------------------------------------------------------
+% write sequences
+%----------------------------------------------------------
+
+nFrames = 1;
+
+% fmri.seq 
+if TODO(1)
+    fn = 'fmri';
+    writeEPI(fn, sys, voxelSize, [nx ny nz], TR, alpha, mb, IY, IZ, ...
+        nFrames, 'SMS', opts);
+    if strcmp(lower(vendor), 'ge')
+        pge2.seq2ge(fn, sysGE, length(IY)*nz/mb, PNSwt);
+    end
+end
+
+% noise.seq
+if TODO(2)
+    fn = 'noise';
+    writeEPI(fn, sys, voxelSize, [nx ny nz], TR, alpha, mb, IY, IZ, ...
+        nFrames, 'SMS', ...
+        pge2.utils.setfields(opts, 'doNoiseScan', true));
+    if strcmp(lower(vendor), 'ge')
+        pge2.seq2ge(fn, sysGE, length(IY)*nz/mb, PNSwt);
+    end
+end
+
+% epical.seq
+%  - EPI ghost calibration
+if TODO(3)
+    fn = 'epical';
+    writeEPI(fn, sys, voxelSize, [nx ny nz], TR*mb, alpha, 1, IY, IZ, ...
+        nFrames, 'SMS', ...
+        pge2.utils.setfields(opts, 'doRefScan', true));
+    if strcmp(lower(vendor), 'ge')
+        pge2.seq2ge(fn, sysGE, round(length(IY)*nz/2), PNSwt);
+    end
+end
+
+% slgcal.seq
+%  - slice GRAPPA calibration
+if TODO(4)
+    fn = 'slgcal';
+    writeEPI(fn, sys, voxelSize, [nx ny nz], TR*mb, alpha, 1, IY, ones(size(IZ)), ...
+        nFrames, 'SMS', opts);
+    if strcmp(lower(vendor), 'ge')
+        pge2.seq2ge(fn, sysGE, round(length(IY)*nz/2), PNSwt);
+    end
+end
+
+% grappacal.seq
+%  - GRAPPA calibration
+if TODO(5)
+    fn = 'grappacal';
+    [IYtmp, IZtmp] = getcaipi(ny, nz, 1, 1, 0, '3DEPI/caipi');
+    writeEPI(fn, sys, voxelSize, [nx ny nz], TR*mb, alpha, 1, IYtmp, IZtmp, ...
+        nFrames, 'SMS', opts);
+    if strcmp(lower(vendor), 'ge')
+        pge2.seq2ge(fn, sysGE, round(length(IYtmp)*nz/2), PNSwt);
+    end
+end
+
+% b0.seq
+%  - 3D B0 field / coil sensitivity maps
+if TODO(6)
+    fn = 'b0';
+    writeB0(fn, ...
+        pge2.utils.setfields(sys, ...
+            'maxSlew', sys.maxSlew*0.5, ...
+            'maxGrad', sys.maxGrad*0.5), ...
+        voxelSize, [nx nx nx], 4);
+    if strcmp(lower(vendor), 'ge')
+        pge2.seq2ge(fn, sysGE, ny, PNSwt);
+    end
+end
+
---
+++ sequence/me/writeB0.m
@@ -0,0 +1,147 @@
+function seq = writeB0(seqName, sys, voxelSize, N, alpha)
+% function seq = writeB0(seqName, sys, voxelSize, N, alpha)
+%
+% Write 3D GRE B0 mapping sequence 
+%
+% Inputs
+%   seqName     string   .seq file name
+%   sys         struct   see mr.opts()
+%   voxelSize   [3]      m
+%   N           [3]      matrix size
+%   alpha       [1]      flip angle [degrees]
+%
+% Outputs
+%   seq         struct   Pulseq sequence object
+
+% --- Input checks ---
+narginchk(5, 5);
+validateattributes(seqName,   {'char','string'}, {'nonempty'}, mfilename, 'seqName');
+validateattributes(sys,       {'struct'},        {'nonempty'}, mfilename, 'sys');
+validateattributes(voxelSize, {'numeric'},       {'vector','numel',3,'real','positive','finite'}, ...
+    mfilename, 'voxelSize');
+validateattributes(N,         {'numeric'},       {'vector','numel',3,'integer','positive','finite'}, ...
+    mfilename, 'N');
+validateattributes(alpha,     {'numeric'},       {'scalar','real','positive','finite'}, ...
+    mfilename, 'alpha');
+assert(N(3) > 1, 'Only 3D scan is supported');
+
+% --- Acquisition parameters ---
+[nx ny nz] = deal(N(1), N(2), N(3));
+fov = voxelSize.*[nx ny nz];
+dwell = 4*sys.adcRasterTime;    % ADC sample time (s)
+fatChemShift = 3.5e-6;          % 3.5 ppm
+fatOffresFreq = sys.gamma*sys.B0*fatChemShift;  % Hz
+TE = 1/fatOffresFreq*[1 2];     % fat and water in phase for both echoes
+
+rfSpoilingInc = 117;            % RF spoiling increment
+nCyclesSpoil = 2;               % number of spoiler cycles
+Tpre = 1.0e-3;                  % prephasing trapezoid duration
+
+% --- Build sequence events ---
+
+% non-selective excitation
+rf = mr.makeBlockPulse(alpha/180*pi, sys, 'Duration', 0.2e-3, 'use', 'excitation');
+
+% gradients and ADC event
+deltak = 1./fov;
+Tread = nx*dwell;
+
+gyPre = mr.makeTrapezoid('y', sys, ...
+    'Area', ny*deltak(2)/2);   % PE1 gradient, max positive amplitude
+gzPre = mr.makeTrapezoid('z', sys, ...
+    'Area', nz*deltak(3)/2);   % PE2 gradient, max amplitude
+
+gx = mr.makeTrapezoid('x', sys, 'Amplitude', nx*deltak(1)/Tread, ... 
+    'FlatTime', Tread);
+gxPre = mr.makeTrapezoid('x', sys, 'Area', -gx.area/2);
+
+adc = mr.makeAdc(nx, sys, 'Duration', Tread, 'Delay', gx.riseTime);
+
+gxSpoil = mr.makeTrapezoid('x', sys, 'Area', nx*deltak(1)*nCyclesSpoil);
+gzSpoil = mr.makeTrapezoid('z', sys, 'Area', nx*deltak(1)*nCyclesSpoil); 
+
+% --- y/z PE steps. Avoid exactly zero ---
+pe1Steps = ((0:ny-1)-ny/2)/ny*2 + 1e-9;
+pe2Steps = ((0:nz-1)-nz/2)/nz*2 + 1e-9;
+
+% --- Calculate timing ---
+TEmin = rf.shape_dur/2 + rf.ringdownTime + mr.calcDuration(gxPre) ...
+      + adc.delay + Tread/2;
+TEdelay = ceil((TE-TEmin)/sys.gradRasterTime)*sys.gradRasterTime;
+if TEdelay < 0
+    TE = 1/fatOffresFreq*[2 3]; 
+    TEdelay = ceil((TE-TEmin)/sys.gradRasterTime)*sys.gradRasterTime;
+end
+
+% --- Loop over phase encodes and define sequence blocks ---
+
+% iz < 0: Dummy shots to reach steady state (ADC off)
+% iz = 0: ADC is turned on and used for receive gain calibration on GE scanners
+% iz > 0: Image acquisition
+
+seq = mr.Sequence(sys);           
+
+nDummyZLoops = 1;
+rf_phase = 0;
+rf_inc = 0;
+
+TRmin = 0;
+for iz = -nDummyZLoops:nz
+    isDummyTR = iz < 0;
+
+    fprintf('\rz encode %d of %d ', iz, nz);
+
+    for iY = 1:ny
+        % Turn on/off y and z prephasing lobes
+        % Avoid scaling to exactly zero so the shapes are preserved in the .seq file
+        yStep = (iz > 0) * pe1Steps(iY) + eps;
+        zStep = (iz > 0) * pe2Steps(max(1,iz)) + eps;
+
+        for c = 1:length(TE)
+
+            % update RF/adc phase (RF spoiling)
+            rf_inc = mod(rf_inc+rfSpoilingInc, 360.0);
+            rf_phase = mod(rf_phase+rf_inc, 360.0);
+            rf.phaseOffset = rf_phase/180*pi;
+            adc.phaseOffset = rf_phase/180*pi;
+
+            % mark start of segment (block group) by adding TRID label
+            seq.addBlock(mr.makeLabel('SET', 'TRID', 1 + isDummyTR));
+
+            % excitation
+            seq.addBlock(rf);
+            
+            % encoding
+            seq.addBlock(mr.makeDelay(TEdelay(c)));   % variable delay block
+            seq.addBlock(gxPre, ...
+                mr.scaleGrad(gyPre, yStep), ...
+                mr.scaleGrad(gzPre, zStep));
+            if isDummyTR
+                seq.addBlock(gx);
+            else
+                seq.addBlock(gx, adc);
+            end
+
+            % rephasing and spoiling
+            seq.addBlock(gxSpoil, ...
+                mr.scaleGrad(gyPre, -yStep), ...
+                mr.scaleGrad(gzPre, -zStep));
+            seq.addBlock(gzSpoil);
+
+            % keep TR constant
+            minVariableDelay = 8e-6;  % mimimum duration of variable delay block (WAIT pulse in EPIC)
+            seq.addBlock(mr.makeDelay(minVariableDelay + TEdelay(end) - TEdelay(c)));
+
+            TRmin = pge2.utils.iff(TRmin, TRmin, seq.duration); % for plotting below
+        end
+    end
+end
+fprintf('\n');
+
+% --- Write .seq file ---
+pge2.utils.writeseq(seq, fov, seqName);
+
+% --- Plot sequence ---
+Noffset = length(TE)*ny*(nDummyZLoops+1);
+seq.plot('timerange',[Noffset Noffset+2*length(TE)]*TRmin, 'timedisp', 'ms');
+
